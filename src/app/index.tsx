5b94a971af45e854ee7a6d6da528f47bd5dc5c70
JGW-Korea <jgw6372@gmail.com>
2025-08-07 19:36:03 +0900

    fix: Expo canary SDK 제거 후 안정 버전으로 전환 및 빌드 오류 해결 (#8)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,15 @@
+import { elevation } from "@/shared/tokens/effect/elevation";
 import Text from "@/shared/tokens/typography/Text";
 import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="flex-1 justify-center items-center" style={elevation.active}>
       <Text variant="display-default">Hello, World!</Text>
       <Text variant="heading-lg">Hello, World!</Text>
+      <Text variant="caption-lg">Hello, World123!</Text>
       <Text variant="caption-lg">Hello, World!</Text>
-      <Text variant="caption-lg">Hello, World!</Text>
-      <Text variant="caption-lg">Hello, World!</Text>
+      <Text variant="caption-lg">Hello, Wor123ld!</Text>
     </View>
   );
 }
