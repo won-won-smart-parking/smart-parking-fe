d6d9d8d19cdd82c5e84e002ef311866472b88b9b
JGW-Korea <jgw6372@gmail.com>
2025-08-09 02:43:42 +0900

    test: warning 수준 코드 삭제

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,9 +3,6 @@ import Text from "@/shared/tokens/typography/Text";
 import { View } from "react-native";
 
 export default function Index() {
-  console.log();
-  console.log();
-
   return (
     <View className="flex-1 justify-center items-center" style={elevation.active}>
       <Text variant="display-default">Hello, World!</Text>
