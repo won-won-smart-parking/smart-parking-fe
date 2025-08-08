79f1e045175b51a536de413a0a94098b42c51030
JGW-Korea <jgw6372@gmail.com>
2025-08-09 02:09:04 +0900

    test: 루트 타입 지정 및 표현식 사용

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,9 +3,11 @@ import Text from "@/shared/tokens/typography/Text";
 import { View } from "react-native";
 
 export default function Index() {
+  let a: number = 123;
+
   return (
     <View className="flex-1 justify-center items-center" style={elevation.active}>
-      <Text variant="display-default">Hello, World!</Text>
+      <Text variant="display-default">{a}</Text>
       <Text variant="heading-lg">Hello, World!</Text>
       <Text variant="caption-lg">Hello, World123!</Text>
       <Text variant="caption-lg">Hello, World!</Text>
