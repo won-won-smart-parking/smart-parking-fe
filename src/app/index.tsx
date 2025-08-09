201459b57cecb472240cf685e1a4321d22310f7d
JGW-Korea <jgw6372@gmail.com>
2025-08-09 17:15:25 +0900

    test: Prettier CI 단계 테스트

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,9 +3,11 @@ import { elevation } from "@/shared/tokens/effect/elevation";
 import Text from "@/shared/tokens/typography/Text";
 
 export default function Index() {
+  let a: number = 123;
+
   return (
     <View className="flex-1 items-center justify-center" style={elevation.active}>
-      <Text variant="display-default">Hello, World!</Text>
+      <Text variant="display-default">{a}</Text>
       <Text variant="heading-lg">Hello, World!</Text>
       <Text variant="caption-lg">Hello, World123!</Text>
       <Text variant="caption-lg">Hello, World!</Text>
