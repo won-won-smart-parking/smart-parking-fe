651edf3bc0dd293308828e41b9f90efc6e2be417
JGW-Korea <jgw6372@gmail.com>
2025-08-11 00:37:34 +0900

    test: 수정된 리포트 내용 확인 테스트 (#123)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
     <View className="flex-1 items-center justify-center" style={elevation.active}>
       <Text variant="display-default">Hello, World!</Text>
       <Text variant="heading-lg">Hello, World!</Text>
-      <Text variant="caption-lg">Hello, World123!</Text>
+      <Text variant="caption-lg">Hello, W123orld123!</Text>
       <Text variant="caption-lg">Hello, World!</Text>
       <Text variant="caption-lg">Hello, Wor123ld!</Text>
     </View>
