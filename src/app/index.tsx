6edfe88371af0bddddb3b8b584ed5d2fbdbbb9c9
JGW-Korea <jgw6372@gmail.com>
2025-08-11 00:10:43 +0900

    test: 자동 리뷰 봇 상세 수정 후 PR 테스트 (#123)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
     <View className="flex-1 items-center justify-center" style={elevation.active}>
       <Text variant="display-default">Hello, World!</Text>
       <Text variant="heading-lg">Hello13123123 World!</Text>
-      <Text variant="caption-lg">Hello, World123!</Text>
+      <Text variant="caption-lg">Hello123131312, World123!</Text>
       <Text variant="caption-lg">Hello, World!</Text>
       <Text variant="caption-lg">Hello,1231312321312 Wor123ld!</Text>
     </View>
