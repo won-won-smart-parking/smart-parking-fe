ee51daba22921f8349edc57f47d9a78e95c5522c
JGW-Korea <jgw6372@gmail.com>
2025-08-13 04:11:38 +0900

    comment: Icon 컴포넌트 주석 내용 수정 (#31)

--- src/shared/ui/atoms/icon/index.tsx
+++ src/shared/ui/atoms/icon/index.tsx
@@ -14,16 +14,15 @@ type Props = Omit<SvgProps, "width" | "height" | "color"> & {
  *
  * 지정한 이름 매개변수 값에 따라 아이콘을 렌더링하는 전역 컴포넌트
  *
- * @param {Object} props - 컴포넌트 속성
- * @param {IconName} [props.name="example"] - 렌더링할 아이콘 이름
- * @param {number} [props.size=24] - 아이콘 크기
- * @param {string} [props.color="#A0A0A0"] - 아이콘 색상 (아직 대기)
+ * @param props.name - 렌더링할 아이콘 이름 (기본값: example)
+ * @param props.size - 아이콘 크기 (기본값: 24)
+ * @param props.color - 아이콘 색상 (아직 대기)
  *
  * @example
  * <Icon name="arrow-button" />
  * <Icon name="edit" size={32} color={color-neutral} />
  *
- * @returns {React.JSX.Element} - 지정된 아이콘 컴포넌트
+ * @returns 지정된 아이콘 컴포넌트
  */
 export default function Icon({ name = "example", size = 24, color = "#A0A0A0", ...rest }: Props) {
   const SvgIcon = registry[name];
