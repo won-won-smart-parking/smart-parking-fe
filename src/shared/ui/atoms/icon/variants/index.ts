b2ba459bbac3c7871acbb44ebd1627cfe718db28
JGW-Korea <jgw6372@gmail.com>
2025-08-13 03:07:38 +0900

    comment: shard/ui/atoms/icon/variants/index.ts keyof typeof 주석 할당 (#31)

--- src/shared/ui/atoms/icon/variants/index.ts
+++ src/shared/ui/atoms/icon/variants/index.ts
@@ -7,4 +7,7 @@ export const registry = {
   ...brandIcons,
 } as const;
 
+// keyof -> 키들의 값을 타입 변환 (⚠️ 런타임 시점에 사라지기 때문에, 타입 변수에만 사용 가능)
+// typeof -> 값을 기준으로 타입 반환 (⚠️ 객체는 Key-Value 형식의 인터페이스 타입 구조로 반환되지만, 단순 변수 선언은 리터럴 타입으로 반환)
+// keyof typeof -> 값을 기준으로 타입 반환 후 해당 타입의 키들의 값으로 타입 반환
 export type IconName = keyof typeof registry; // 모듈 집합 객체 키를 기준으로 타입 선언
