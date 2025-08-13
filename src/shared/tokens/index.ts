9f0a3d2c4099a8ff1ec61b069ef28606e0b55bdc
JGW-Korea <jgw6372@gmail.com>
2025-08-14 05:39:25 +0900

    feat: Spacing 디자인 토큰값 정의 (#46)

--- src/shared/tokens/index.ts
+++ src/shared/tokens/index.ts
@@ -1,2 +1,5 @@
 export { default as colors } from "./color";
+export { default as fontSize } from "./font-size";
+export { default as spacing } from "./spacing";
+
 export { default as elevation } from "./elevation";
