69675ef7e3c481bd3e2a1f44f17919c598a2e84b
JGW-Korea <jgw6372@gmail.com>
2025-08-13 19:04:39 +0900

    feat: NativeWind ClassName -> width / color prop 매핑 적용 (#31)

--- src/shared/ui/atoms/icon/index.tsx
+++ src/shared/ui/atoms/icon/index.tsx
@@ -3,10 +3,9 @@ import { IconName, registry } from "./variants";
 
 // Atom / Icon 속성 타입 정의 (SvgProps 이용)
 // Omit<T, K> 유니온 타입 -> T(TYPE)에서 K(KEY) 속성을 생략하고, 나머지를 선택한 새로운 타입 반환
-type Props = Omit<SvgProps, "width" | "height" | "color"> & {
+type Props = Omit<SvgProps, "height" | "style"> & {
   name?: IconName; // 기본값 = example
   size?: number; // 기본값 = 24
-  color?: string; // 기본값 = #A0A0A0
 };
 
 /**
@@ -15,17 +14,16 @@ type Props = Omit<SvgProps, "width" | "height" | "color"> & {
  * 지정한 이름 매개변수 값에 따라 아이콘을 렌더링하는 전역 컴포넌트
  *
  * @param props.name - 렌더링할 아이콘 이름 (기본값: example)
- * @param props.size - 아이콘 크기 (기본값: 24)
- * @param props.color - 아이콘 색상 (아직 대기)
+ * @param props.className - 아이콘의 사이즈, 색상을 지정할 수 있는 NativeWind 코드 입력 (허용값: w-*, color-*)
  *
  * @example
  * <Icon name="arrow-button" />
- * <Icon name="edit" size={32} color={color-neutral} />
+ * <Icon name="edit" className="w-4 color-neutral-300" />
  *
  * @returns 지정된 아이콘 컴포넌트
  */
-export default function Icon({ name = "example", size = 24, color = "#A0A0A0", ...rest }: Props) {
-  const SvgIcon = registry[name];
+export default function Icon({ name = "example", width: size = 24, color, ...rest }: Props) {
+  const SvgIcon = registry[name]; // name에 맞는 실제 SVG 선택 (컴포넌트 변수)
 
   // prettier-ignore
   return (
