b0663f0cd6292fddce0e69fb909524d8cabbf987
JGW-Korea <jgw6372@gmail.com>
2025-08-15 13:16:16 +0900

    feat: 버튼 variant별 ButtonContent 컴포넌트 구조 정의 (#27)

---
+++ src/shared/ui/atoms/button/content.tsx
@@ -0,0 +1,50 @@
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { Variant as ButtonVariant, VariantKey as ButtonVariantKey } from "./variant";
+import Icon from "../icon";
+import Text from "../text";
+
+// ButtonContent 타입 선언
+// - variant(union): "label" | "icon" | "both"
+// - content(object): ButtonVariant[variant]
+// interface Props {
+//   variant: ButtonVariantKey;
+//   content: ButtonVariant;
+// }
+// -> 이렇게 하면 문제가 ButtonContent를 호출하는 쪽에서 모든 ButtonVariant를 구성을 다 보내줘야됨
+
+// 맵드(Mapped) 타입을 통해 타입 실체 구조를 다음과 같이 변경
+// {
+//   label: { variant: 'label'; content: ButtonVariant['label'] };
+//   icon:  { variant: 'icon';  content: ButtonVariant['icon']  };
+//   both:  { variant: 'both';  content: ButtonVariant['both']  };
+// }
+// 인덱스드 엑세스 타입(Indexed Access Type)을 통해 타입 객체의 구조를 가지고 옴
+// { ...위 구조 }["label" | "icon" | "both"]
+// Props
+// {
+//   { variant: 'label'; content: ButtonVariant['label'] } |
+//   { variant: 'icon';  content: ButtonVariant['icon']  } |
+//   { variant: 'both';  content: ButtonVariant['both']  }
+// }
+type Props = {
+  [K in ButtonVariantKey]: { variant: K; content: ButtonVariant[K] };
+}[ButtonVariantKey];
+
+export default function ButtonConten({ variant, content }: Props) {
+  // 버튼 내부 콘텐츠의 구조를 정의
+  switch (variant) {
+    case "label":
+      return <Text accessibilityLabel={content.accessibilityLabel}>{content.text}</Text>;
+    case "icon":
+      return <Icon name={content.iconName} accessibilityLabel={content.accessibilityLabel} />;
+    case "both":
+      return (
+        <View accessibilityLabel={content.accessibilityLabel}>
+          <Icon name={content.iconName} />
+          <Text>{content.text}</Text>
+        </View>
+      );
+    default:
+      return null;
+  }
+}
