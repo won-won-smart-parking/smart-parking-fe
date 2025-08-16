26a5752fade558039104e001ef04e8cd0eb0d69b
JGW-Korea <jgw6372@gmail.com>
2025-08-17 02:02:21 +0900

    comment: Atom / Radio 컴포넌트 JSDoc 주석 내용 작성 (#36)

--- src/shared/ui/atoms/radio/index.tsx
+++ src/shared/ui/atoms/radio/index.tsx
@@ -3,9 +3,26 @@ import { Pressable, PressableProps, View } from "react-native";
 import { elevation } from "@shared/tokens";
 
 interface Props extends PressableProps {
-  selected: boolean;
+  selected?: boolean;
 }
 
+/**
+ * Atom / Radio
+ *
+ * Radio Item 내부에서 사용되는 순수 시각적 컴포넌트입니다.
+ * 선택 여부에 따라 스타일만 변경되며, 비즈니스 로직이나 접근성 역할은 포함하지 않습니다.
+ *
+ * @param props.selected  선택 상태를 나타내는 값 (기본값: false)
+ *
+ * @example
+ * // 선택된 상태
+ * <Radio selected={true} />
+ *
+ * // 선택되지 않은 상태
+ * <Radio selected={false} />
+ *
+ * @returns ReactElement Radio Component
+ */
 export default function Radio({ selected = false }: Props) {
   return (
     <Pressable className="aspect-square w-7 overflow-hidden rounded-full">
