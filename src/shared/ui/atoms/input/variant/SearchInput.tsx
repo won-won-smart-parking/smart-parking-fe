22495994af83f451da209052dd080d78a553c09a
JGW-Korea <jgw6372@gmail.com>
2025-08-18 01:26:28 +0900

    comment: Search Input JSDoc 주석 내용 추가 (#32)

--- src/shared/ui/atoms/input/variant/SearchInput.tsx
+++ src/shared/ui/atoms/input/variant/SearchInput.tsx
@@ -4,6 +4,27 @@ import ClearButton from "../part/ClearButton";
 
 type Props = InputRequiredProps & Omit<TextInputProps, keyof InputRequiredProps>;
 
+/**
+ * Atom / Input (Search Input)
+ *
+ * 검색용 Input 입니다. 기본 입력 필드와 Clear 버튼만 제공하는 컴포넌트입니다.
+ * 비즈니스 로직은 포함하지 않고, 외부 상태와 이벤트 핸들러를 전달받아 동작합니다.
+ *
+ * @param props.value         입력 값
+ * @param props.placeholder   안내 문구
+ * @param props.onChangeText  텍스트 변경 이벤트 핸들러
+ * @param props.onPress       Clear 버튼 눌림(초기화) 이벤트 핸들러
+ *
+ * @example
+ * <SearchInput
+ *   value={query}
+ *   placeholder="검색어를 입력하세요"
+ *   onChangeText={setQuery}
+ *   onPress={() => setQuery("")}
+ * />
+ *
+ * @returns ReactElement SearchInput Component
+ */
 export default function SearchInput({ value = "", placeholder = "placeholder", onChangeText, onPress }: Props) {
   return (
     <View className={InputStyle.container}>
