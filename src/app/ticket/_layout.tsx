a2c48283a6dc10d3c55d1b2dfd6d01beae411db0
JGW-Korea <jgw6372@gmail.com>
2025-08-18 11:06:46 +0900

    feat: /ticket 뒤로가기 동작 및 헤더 타이틀 수정 (#39)

--- src/app/ticket/_layout.tsx
+++ src/app/ticket/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack, useRouter } from "expo-router";
 import { Pressable } from "react-native";
 import Icon from "@/shared/ui/atoms/icon";
+import Text from "@/shared/ui/atoms/text";
 
 export default function TicketLayout() {
   const router = useRouter();
@@ -12,14 +13,24 @@ export default function TicketLayout() {
         headerStyle: { height: 128 },
         headerBackTitleVisible: false,
         headerLeft: () => (
-          <Pressable onPress={() => router.back()}>
+          <Pressable onPress={() => router.dismissAll()}>
             <Icon name="arrowLeft" className="w-4 text-neutral-850" />
           </Pressable>
         ),
       }}
     >
-      <Stack.Screen name="history" />
-      <Stack.Screen name="[passId]" />
+      <Stack.Screen
+        name="history"
+        options={{
+          headerTitle: () => <Text variant="heading-md">이용내역</Text>,
+        }}
+      />
+      <Stack.Screen
+        name="[passId]"
+        options={{
+          headerTitle: () => <Text variant="heading-md">이용권 상세</Text>,
+        }}
+      />
     </Stack>
   );
 }
