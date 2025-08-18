2c5d59de6cca1e5fa82332ad27d6da6848d89511
JGW-Korea <jgw6372@gmail.com>
2025-08-18 10:10:02 +0900

    refactor: TopTabs tabBarLabel을 Atom / Text로 변경 및 스타일 수정 (#39)

--- src/app/(tabs)/ticket/_layout.tsx
+++ src/app/(tabs)/ticket/_layout.tsx
@@ -1,4 +1,6 @@
+import clsx from "clsx";
 import { withLayoutContext } from "expo-router";
+import Text from "@/shared/ui/atoms/text";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 const { Navigator } = createMaterialTopTabNavigator();
@@ -7,9 +9,27 @@ export const TopTabs = withLayoutContext(Navigator);
 
 export default function TopTabsLayout() {
   return (
-    <TopTabs screenOptions={{ tabBarIndicatorStyle: { backgroundColor: "blue" } }}>
-      <TopTabs.Screen name="daily" options={{ title: "일일권" }} />
-      <TopTabs.Screen name="regular" options={{ title: "정기권" }} />
+    <TopTabs screenOptions={{ tabBarIndicatorStyle: { backgroundColor: "#5BABF8" } }}>
+      <TopTabs.Screen
+        name="daily"
+        options={{
+          tabBarLabel: ({ focused }: { focused: boolean }) => (
+            <Text variant="label-sm" className={clsx(!focused ? "text-neutral-850" : "text-blue-300")}>
+              일일권
+            </Text>
+          ),
+        }}
+      />
+      <TopTabs.Screen
+        name="regular"
+        options={{
+          tabBarLabel: ({ focused }: { focused: boolean }) => (
+            <Text variant="label-sm" className={clsx(!focused ? "text-neutral-850" : "text-blue-300")}>
+              정기권
+            </Text>
+          ),
+        }}
+      />
     </TopTabs>
   );
 }
