fc5a29fcc9d61645dadd3d28f331ff9a5595cb31
JGW-Korea <jgw6372@gmail.com>
2025-08-21 00:37:14 +0900

    refactor: Base Button 컴포넌트 구성 (#63)

--- src/shared/ui/refactor/button/foundation/BsaeButton.tsx
+++ src/shared/ui/refactor/button/foundation/BsaeButton.tsx
@@ -1,1 +1,59 @@
-export default function BaseButton() {}
+import clsx from "clsx";
+import { ReactNode } from "react";
+import { Pressable, PressableProps, View } from "react-native";
+import { elevation } from "@/shared/tokens";
+import { ButtonContainerPalette, defaultPalette } from "./styles";
+
+// 공통 버튼 컴포넌트 Props 구성
+export interface Props extends PressableProps {
+  fullWidth?: boolean;
+  border?: boolean;
+  roundedFull?: boolean;
+  disabled?: boolean;
+  disablePressedEffect?: boolean;
+  overrideButtonContainerStyles?: string;
+  palette?: Partial<ButtonContainerPalette>;
+  children: ReactNode;
+}
+
+// 전역적으로 재사용하는 버튼 컴포넌트
+export default function BaseButton({
+  disabled = false,
+  fullWidth = true,
+  border = false,
+  roundedFull = false,
+  disablePressedEffect = false,
+  overrideButtonContainerStyles = "",
+  palette,
+  onPress,
+  children,
+}: Props) {
+  return (
+    <Pressable
+      disabled={disabled}
+      className={clsx(fullWidth ? "w-full" : "w-auto")}
+      onPress={disabled ? null : onPress}
+    >
+      {({ pressed }) => {
+        const mergedPalette = { ...defaultPalette(border), ...palette };
+
+        return (
+          <View
+            className={clsx(
+              "flex items-center justify-center px-4 py-3",
+              border && "border",
+              roundedFull ? "rounded-full" : "rounded-[8px]",
+              pressed
+                ? clsx(mergedPalette.bgPressedColor, mergedPalette.borderPressedColor)
+                : clsx(mergedPalette.bgColor, mergedPalette.borderColor),
+              overrideButtonContainerStyles,
+            )}
+            style={!disablePressedEffect && pressed ? elevation.active : null}
+          >
+            {children}
+          </View>
+        );
+      }}
+    </Pressable>
+  );
+}
