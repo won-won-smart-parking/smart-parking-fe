a77b87c9aeffc5c343874749010268b754b4993b
JGW-Korea <jgw6372@gmail.com>
2025-08-22 14:31:37 +0900

    fix: Main Page Atom / Text 잘못 작성된 Props 속성명 수정 (#63)

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import Text from "@/shared/ui/atoms/text";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text variant="display-default">메인 페이지</Text>
+      <Text typography="display-default">메인 페이지</Text>
     </View>
   );
 }
