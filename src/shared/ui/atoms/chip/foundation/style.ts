750b1d87b9e05c6944aab995153927e0b153f416
JGW-Korea <jgw6372@gmail.com>
2025-08-22 12:57:00 +0900

    fix: Atom / Chip variant 구성 옵션에 따른 Style 변경 조건 삭제 (#63)

--- src/shared/ui/atoms/chip/foundation/style.ts
+++ src/shared/ui/atoms/chip/foundation/style.ts
@@ -1,33 +1,7 @@
-// Chip 팔레트 기본값 -> 잘못된 분기를 통한 기본값 반환
-// - Button 컴포넌트에서 팔레트 기본값이 사용은 되고 있지만, 그래도 undefined 반환보다는 예기치 않은 오류에 대해 안전하다고 판단하에 작성
-const getDefaultChipPalette = (border: boolean) => {
-  return border
-    ? {
-        bgColor: "bg-neutral-100",
-        bgPressedColor: "bg-neutral-300",
-        borderColor: "border-coolgray-200",
-        borderPressedColor: "border-coolgray-300",
-        textColor: "text-neutral-900",
-        textPressedColor: "text-neutral-870",
-        iconColor: "text-neutral-900",
-        iconPressedColor: "text-neutral-870",
-      }
-    : {
-        bgColor: "bg-coolgray-100",
-        bgPressedColor: "bg-coolgray-300",
-        borderColor: "border-transparent", // border 없는 경우도 명시
-        borderPressedColor: "border-transparent",
-        textColor: "text-neutral-900",
-        textPressedColor: "text-neutral-870",
-        iconColor: "text-neutral-900",
-        iconPressedColor: "text-neutral-870",
-      };
-};
-
 // Chip 분기에 따른 팔레트 반환
-export const chipPalette = (variant: "label" | "icon" | "both", border: boolean, selected: boolean) => {
+export const chipPalette = (border: boolean, selected: boolean) => {
   // border가 true면서, label, icon, both인 경우
-  if (border && ["label", "icon", "both"].includes(variant)) {
+  if (border) {
     // 선택된 상태
     if (selected) {
       return {
@@ -56,7 +30,7 @@ export const chipPalette = (variant: "label" | "icon" | "both", border: boolean,
   }
 
   // border가 false면서, both인 경우
-  else if (!border && variant === "both") {
+  else {
     // 선택된 상태
     if (selected) {
       return {
@@ -79,6 +53,4 @@ export const chipPalette = (variant: "label" | "icon" | "both", border: boolean,
       iconPressedColor: "text-neutral-900",
     };
   }
-
-  return getDefaultChipPalette(border); // 조건이 모두 부합하지 않은 분기일 경우 팔레트 기본값 반환
 };
