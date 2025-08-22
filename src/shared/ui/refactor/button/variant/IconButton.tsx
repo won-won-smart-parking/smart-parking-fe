e6bfae63f2954a19dfe5403c991386d21a555b03
JGW-Korea <jgw6372@gmail.com>
2025-08-22 09:25:43 +0900

    refactor: Icon-Only Button 컴포넌트 구성 (#63)

--- src/shared/ui/refactor/button/variant/IconButton.tsx
+++ src/shared/ui/refactor/button/variant/IconButton.tsx
@@ -1,1 +1,47 @@
-export default function IconButton() {}
+import clsx from "clsx";
+import { IconName } from "@/shared/ui/atoms/icon/variants";
+import Icon from "@shared/ui/atoms/icon";
+import { BaseButton, BaseButtonProps, ButtonContainerPalette } from "../foundation";
+
+interface Palette extends ButtonContainerPalette {
+  iconColor: string;
+  iconPressed: string;
+}
+
+interface Props extends BaseButtonProps {
+  iconName: IconName;
+  palette?: Partial<Palette>;
+}
+
+export default function IconButton({
+  iconName,
+  fullWidth,
+  border,
+  roundedFull,
+  disabled,
+  disablePressedEffect,
+  overrideButtonContainerStyles,
+  palette,
+  onPress,
+}: Props) {
+  return (
+    <BaseButton
+      fullWidth={fullWidth}
+      border={border}
+      roundedFull={roundedFull}
+      disabled={disabled}
+      disablePressedEffect={disablePressedEffect}
+      overrideButtonContainerStyles={overrideButtonContainerStyles}
+      palette={palette}
+      onPress={onPress}
+      renderContent={(pressed) => (
+        <Icon
+          name={iconName}
+          className={clsx(
+            !pressed ? palette?.iconColor || "text-neutral-900" : palette?.iconPressed || "text-neutral-870",
+          )}
+        />
+      )}
+    />
+  );
+}
