620e8e56f97ed3311a5d29ccc8f1b1773e498e19
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-08-24 02:48:26 +0900

    refactor: avatarSizeStyle 공통 타입 설정 (#26)

--- src/shared/ui/atoms/avatar/avatar-styles.ts
+++ src/shared/ui/atoms/avatar/avatar-styles.ts
@@ -1,11 +1,7 @@
-export const avatarSize = {
-  sm: "w-[32px] h-[32px]",
-  md: "w-[72px] h-[72px]",
-  lg: "w-[100px] h-[100px]",
-} as const;
+export type AvatarSize = "sm" | "md" | "lg";
 
-export const avatarBorder = {
-  sm: "border-1 border-neutral-300",
-  md: "border-2 border-neutral-300",
-  lg: "border-4 border-neutral-300",
+export const avatarSizeStyle = {
+  sm: "w-8 h-8 border",
+  md: "w-18 h-18 border-2",
+  lg: "w-25 h-25 border-4",
 } as const;
--- src/shared/ui/atoms/avatar/index.tsx
+++ src/shared/ui/atoms/avatar/index.tsx
@@ -1,24 +1,23 @@
 import { Image, View } from "react-native";
 import Icon from "@/shared/ui/atoms/icon";
-import { avatarBorder, avatarSize } from "./avatar-styles";
+import { type AvatarSize, avatarSizeStyle } from "./avatar-styles";
 
 interface Props {
-  size?: "sm" | "md" | "lg"; // 아바타 크기
-  image?: string; // 외부에서 받아오는 이미지 URL
+  size?: AvatarSize; // 아바타 크기
+  profileUrl?: string; // 외부에서 받아오는 이미지 URL
 }
 
-export default function Avatar({ size = "md", image }: Props) {
-  const sizeStyle = avatarSize[size]; // 크기 (32 / 72 / 100)
-  const borderStyle = avatarBorder[size]; // border 두께 (1 / 2 / 4, neutral-300)
+export default function Avatar({ size = "md", profileUrl }: Props) {
+  const sizeStyle = avatarSizeStyle[size];
 
   return (
-    <View className={`${sizeStyle} ${borderStyle} items-center justify-center rounded-full bg-coolgray-300`}>
-      {image ? (
+    <View className={`${sizeStyle} items-center justify-center rounded-full border-neutral-300 bg-coolgray-300`}>
+      {profileUrl ? (
         // 이미지 등록될 경우
-        <Image source={{ uri: image }} className="h-full w-full rounded-full" resizeMode="cover" />
+        <Image source={{ uri: profileUrl }} className="h-full w-full rounded-full" resizeMode="cover" />
       ) : (
         // 이미지가 없으면, 기본 아이콘
-        <Icon name="userOutlined" className="h-1/2 w-1/2 text-coolgray-500" />
+        <Icon name="userOutlined" className="w-1/2 text-coolgray-500" />
       )}
     </View>
   );
