4299d89549dd260d802111acc6c72afb29fe2e12
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-08-23 22:04:00 +0900

    feat: 이미지 없을 때 기본 userOutlined 아이콘 표시 (#26)

--- src/shared/ui/atoms/avatar/index.tsx
+++ src/shared/ui/atoms/avatar/index.tsx
@@ -1,4 +1,5 @@
 import { Image, View } from "react-native";
+import Icon from "@/shared/ui/atoms/icon";
 import { avatarBorder, avatarSize } from "./avatar-styles";
 
 interface AvatarProps {
@@ -12,7 +13,11 @@ export default function Avatar({ size = "md", image }: AvatarProps) {
 
   return (
     <View className={`${sizeStyle} ${borderStyle} items-center justify-center rounded-full bg-coolgray-300`}>
-      {image && <Image source={{ uri: image }} className="h-full w-full rounded-full" resizeMode="cover" />}
+      {image ? (
+        <Image source={{ uri: image }} className="h-full w-full rounded-full" resizeMode="cover" />
+      ) : (
+        <Icon name="userOutlined" className="h-1/2 w-1/2 text-coolgray-500" />
+      )}
     </View>
   );
 }
