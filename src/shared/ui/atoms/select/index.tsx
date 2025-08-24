aa9ee5f1c6b46e6722c16d0006d9951f519fb31c
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-08-25 02:53:07 +0900

    feat: Select 컴포넌트 배리언트에 따른 스타일 구현 (#37)

--- src/shared/ui/atoms/select/index.tsx
+++ src/shared/ui/atoms/select/index.tsx
@@ -1,20 +1,34 @@
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 import Icon from "@/shared/ui/atoms/icon";
-import Text from "@/shared/ui/atoms/text";
-
-export type SelectLabel = "시간설정" | "날짜설정";
+import Text from "@shared/ui/atoms/text";
 
 interface Props {
-  label: SelectLabel;
+  label: string;
+  value?: string;
+  onPress?: () => void;
 }
 
-export default function Select({ label }: Props) {
+export default function Select({ label, value, onPress }: Props) {
+  const selected = !!value;
+
   return (
-    <View className="flex-row items-center justify-center">
-      <Text typography="caption-tight" className="text-neutral-800">
-        {label}
-      </Text>
-      <Icon name="arrowBottomFill" className="text-neutral-950" />
-    </View>
+    <Pressable onPress={onPress}>
+      {({ pressed }) => {
+        // 배경색
+        const bgColor = selected && pressed ? "bg-neutral-300" : !selected && pressed ? "bg-neutral-200" : "";
+
+        // 텍스트 색상
+        const textColor = selected ? "text-neutral-950" : "text-neutral-800";
+
+        return (
+          <View className={`w-auto flex-row items-center justify-center ${bgColor}`}>
+            <Text typography={selected ? "caption-xxl" : "caption-tight"} className={textColor}>
+              {value || label}
+            </Text>
+            <Icon name="arrowBottomFill" className="text-neutral-950" />
+          </View>
+        );
+      }}
+    </Pressable>
   );
 }
