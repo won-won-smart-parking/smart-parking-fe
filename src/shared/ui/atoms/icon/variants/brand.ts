bd0fde685fec4775427bd2347eb52549d3d3d1cb
JGW-Korea <jgw6372@gmail.com>
2025-08-25 16:20:48 +0900

    refactor: shared/assets/icon -> icons 디렉토리명 수정 및 Icon 경로 수정 (#78)

--- src/shared/ui/atoms/icon/variants/brand.ts
+++ src/shared/ui/atoms/icon/variants/brand.ts
@@ -1,15 +1,15 @@
 // 브랜드 SVG 파일 import
-import Apple from "@shared/assets/icon/brand/apple.svg";
-import Facebook from "@shared/assets/icon/brand/facebook.svg";
-import GoogleMap from "@shared/assets/icon/brand/google-map.svg";
-import KakaoMap from "@shared/assets/icon/brand/kakao-map.svg";
-import Kakao from "@shared/assets/icon/brand/kakao.svg";
-import Kakaopay from "@shared/assets/icon/brand/kakaopay.svg";
-import NaverMap from "@shared/assets/icon/brand/naver-map.svg";
-import Naver from "@shared/assets/icon/brand/naver.svg";
-import Naverpay from "@shared/assets/icon/brand/naverpay.svg";
-import TMAP from "@shared/assets/icon/brand/tmap.svg";
-import Tosspay from "@shared/assets/icon/brand/tosspay.svg";
+import Apple from "@shared/assets/icons/brand/apple.svg";
+import Facebook from "@shared/assets/icons/brand/facebook.svg";
+import GoogleMap from "@shared/assets/icons/brand/google-map.svg";
+import KakaoMap from "@shared/assets/icons/brand/kakao-map.svg";
+import Kakao from "@shared/assets/icons/brand/kakao.svg";
+import Kakaopay from "@shared/assets/icons/brand/kakaopay.svg";
+import NaverMap from "@shared/assets/icons/brand/naver-map.svg";
+import Naver from "@shared/assets/icons/brand/naver.svg";
+import Naverpay from "@shared/assets/icons/brand/naverpay.svg";
+import TMAP from "@shared/assets/icons/brand/tmap.svg";
+import Tosspay from "@shared/assets/icons/brand/tosspay.svg";
 
 // 브랜드 컴포넌트 상수 타입 단언을 통해 내보내기
 export const brandIcons = {
