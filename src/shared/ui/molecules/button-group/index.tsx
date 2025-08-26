43fcaf6cb28b4f67499af738dc359d5b9d2c2eb2
JGW-Korea <jgw6372@gmail.com>
2025-08-26 18:53:13 +0900

    feat: Molecular / Button Group 스타일 적용 (#85)

--- src/shared/ui/molecules/button-group/index.tsx
+++ src/shared/ui/molecules/button-group/index.tsx
@@ -2,9 +2,13 @@ import clsx from "clsx";
 import { View, ViewProps } from "react-native";
 
 interface Props extends Required<Pick<ViewProps, "children">> {
-  direction: "column" | "row";
+  direction?: "column" | "row";
 }
 
-export default function ButtonGroup({ direction, children }: Props) {
-  return <View className={clsx("", direction === "row" ? "flex-row" : "flex-col")}>{children}</View>;
+export default function ButtonGroup({ direction = "row", children }: Props) {
+  return (
+    <View className={clsx("items-center justify-around", direction === "row" ? "flex-row" : "flex-col")}>
+      {children}
+    </View>
+  );
 }
