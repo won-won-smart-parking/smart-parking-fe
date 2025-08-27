6217438427c506b0a67a395497a30ce4abb8c4fa
JGW-Korea <jgw6372@gmail.com>
2025-08-27 18:06:58 +0900

    refactor: Button Group flex-continaer 주축 스타일 justify-between으로 수정 (#85)

--- src/shared/ui/molecules/button-group/index.tsx
+++ src/shared/ui/molecules/button-group/index.tsx
@@ -37,7 +37,7 @@ interface Props extends Required<Pick<ViewProps, "children">> {
  */
 export default function ButtonGroup({ direction = "row", children }: Props) {
   return (
-    <View className={clsx("items-center justify-around", direction === "row" ? "flex-row" : "flex-col")}>
+    <View className={clsx("items-center justify-between", direction === "row" ? "flex-row" : "flex-col")}>
       {children}
     </View>
   );
