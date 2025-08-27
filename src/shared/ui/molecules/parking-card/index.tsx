de3e8652802cc59226e95d59fc5d335dcf34c5bb
JGW-Korea <jgw6372@gmail.com>
2025-08-27 18:04:23 +0900

    feat: Molecular / Parking Card 주차장 설명 공통 컴포넌트 추상화 및 적용 (#84)

--- src/shared/ui/molecules/parking-card/index.tsx
+++ src/shared/ui/molecules/parking-card/index.tsx
@@ -1,9 +1,18 @@
 import { Image, View } from "react-native";
+import { ParkingStatus } from "@shared/types/parking-status";
 import { Tag, Text } from "@shared/ui/atoms";
 import ParkingCardActionButton from "./part/ParkingCardActionButton";
+import ParkingCardDescription from "./part/ParkingCardDescription";
 import ButtonGroup from "../button-group";
 
-export default function ParkingCard() {
+interface Props {
+  parkingTitle: string; // 주차장 이름
+  parkingStatus: ParkingStatus; // 주차장 혼잡도 상태
+  parkingType?: "public" | "private"; // 주차장 구분
+  parkingImageUrl: string;
+}
+
+export default function ParkingCard({ parkingTitle, parkingStatus, parkingType, parkingImageUrl }: Props) {
   return (
     <View className="gap-4 pb-6 pt-4">
       {/* 주자창 정보 레이아웃 */}
@@ -11,20 +20,21 @@ export default function ParkingCard() {
         <View className="gap-1.5">
           <View className="flex-row justify-between">
             <View>
-              <Text typography="title-md">Parking Title</Text>
+              <Text typography="title-md">{parkingTitle}</Text>
               {/* 전기차 충전, CCTV 유무 등 옵션 */}
             </View>
-            <Tag status="available" />
+
+            <Tag status={parkingStatus} />
           </View>
 
           <View className="gap-0.5">
-            <Text>Parking Type</Text>
-            <Text>Status ･ Distance ･ Capacity (Available / Totla) </Text>
+            <ParkingCardDescription>{parkingType === "public" ? "공용주차장" : "민영주차장"}</ParkingCardDescription>
+            <ParkingCardDescription>Status ･ Distance ･ Capacity (Available / Totla)</ParkingCardDescription>
           </View>
         </View>
 
         {/* 주차장 이미지 */}
-        <Image />
+        <Image source={{ uri: parkingImageUrl }} alt={parkingTitle} className="h-full w-full" />
       </View>
 
       {/* 주차장 버튼 그룹 */}
