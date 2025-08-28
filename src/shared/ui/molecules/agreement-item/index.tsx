13dd5f56ee2f8d4a69d4cfad28fb2c11ed2a2e38
JGW-Korea <jgw6372@gmail.com>
2025-08-28 19:10:26 +0900

    comment: Molecular / Aggrement Item JSDoc 주석 내용 추가 (#91)

--- src/shared/ui/molecules/agreement-item/index.tsx
+++ src/shared/ui/molecules/agreement-item/index.tsx
@@ -2,17 +2,35 @@ import { Pressable, PressableProps } from "react-native";
 import { Checkbox, Text } from "@shared/ui/atoms";
 
 interface Props extends Required<Pick<PressableProps, "onPress">> {
-  label: string;
+  description: string;
   selected: boolean;
 }
 
-export default function AgreementItem({ label, selected, onPress }: Props) {
+/**
+ * Molecular / Agreement Item
+ *
+ * Atom / Checkbox + Text를 조합하여 만든 동의 항목 리스트 아이템 컴포넌트입니다. 비즈니스 로직은 포함하지 않습니다.
+ *
+ * @param props.description  동의 항목에 대한 설명 문구
+ * @param props.selected     해당 동의 항목이 선택되었는지 여부
+ * @param props.onPress      동의 항목을 눌렀을 때 호출되는 이벤트 핸들러
+ *
+ * @example
+ * <Pressable
+ *  description={"description >"}
+ *  selected={selectedState}
+ *  onPress={() => ...}
+ * />
+ *
+ * @returns ReactElement Molecular / Agreement Item
+ */
+export default function AgreementItem({ description, selected, onPress }: Props) {
   return (
     <Pressable className="flex-row items-center gap-3" onPress={onPress}>
       {({ pressed }) => (
         <>
           <Checkbox selected={selected} pressed={pressed} />
-          <Text typography="description-md">{label}</Text>
+          <Text typography="description-md">{description}</Text>
         </>
       )}
     </Pressable>
