3a558122b8fbf09918d1d2070a7099357e5bb668
JGW-Korea <jgw6372@gmail.com>
2025-08-28 19:40:46 +0900

    comment: Molecular / Radio Item JSDoc 주석 내용 추가 (#93)

--- src/entities/payment/ui/molecules/RadioItem.tsx
+++ src/entities/payment/ui/molecules/RadioItem.tsx
@@ -5,14 +5,33 @@ import { IconName } from "@shared/ui/atoms/icon/variant";
 interface Props extends Required<Pick<PressableProps, "onPress">> {
   label: string;
   iconName: IconName;
+  selected: boolean;
 }
 
-export default function RadioItem({ label, iconName, onPress }: Props) {
+/**
+ * Molecular / Radio Item
+ *
+ * Atom / Radio + Text를 조합하여 만든 Radio 리스트 아이템 컴포넌트입니다. 비즈니스 로직은 포함하지 않습니다.
+ *
+ * @param props.label        동의 항목에 대한 설명 문구
+ * @param props.selected     해당 동의 항목이 선택되었는지 여부
+ * @param props.onPress      동의 항목을 눌렀을 때 호출되는 이벤트 핸들러
+ *
+ * @example
+ * <RadioItem
+ *  label={"label >"}
+ *  selected={selectedState}
+ *  onPress={() => ...}
+ * />
+ *
+ * @returns ReactElement Molecular / Radio Item
+ */
+export default function RadioItem({ label, iconName, selected, onPress }: Props) {
   return (
     <Pressable className="flex-row gap-3" onPress={onPress}>
       {({ pressed }) => (
         <>
-          <Radio pressed={pressed} />
+          <Radio selected={selected} pressed={pressed} />
           <View className="flex-row items-center gap-2">
             <Icon name={iconName} />
             <Text typography="description-md">{label}</Text>
