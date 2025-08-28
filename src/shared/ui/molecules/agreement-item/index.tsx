181e8699dce5bd98427bfc29d221143958360c4c
JGW-Korea <jgw6372@gmail.com>
2025-08-28 18:33:15 +0900

    feat: Molecular / Agreement Item 기본 레이아웃 구축 (#91)

---
+++ src/shared/ui/molecules/agreement-item/index.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { Checkbox, Text } from "@shared/ui/atoms";
+
+export default function AgreementItem() {
+  return (
+    <View>
+      <Checkbox />
+      <Text></Text>
+    </View>
+  );
+}
