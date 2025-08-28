09c4be3e3ad711d9a96134b7581f5980d3a6ea8b
JGW-Korea <jgw6372@gmail.com>
2025-08-28 18:53:49 +0900

    feat: Molecular / Agreement Item Props 구조 구성 및 스타일 적용 (#91)

--- src/shared/ui/molecules/agreement-item/index.tsx
+++ src/shared/ui/molecules/agreement-item/index.tsx
@@ -1,11 +1,22 @@
-import { View } from "react-native";
+import { Pressable, PressableProps } from "react-native";
 import { Checkbox, Text } from "@shared/ui/atoms";
 
-export default function AgreementItem() {
+interface Props extends Required<Pick<PressableProps, "onPress">> {
+  label: string;
+  selected: boolean;
+}
+
+export default function AgreementItem({ label, selected, onPress }: Props) {
   return (
-    <View>
-      <Checkbox />
-      <Text></Text>
-    </View>
+    <Pressable className="flex-row items-center gap-3" onPress={onPress}>
+      {() => (
+        <>
+          <Checkbox selected={selected} />
+          <Text typography="description-md">{label}</Text>
+        </>
+      )}
+    </Pressable>
   );
 }
+
+// { pressed }
