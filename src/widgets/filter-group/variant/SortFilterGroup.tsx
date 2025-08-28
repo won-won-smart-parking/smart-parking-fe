a33d5f43cae81f3c8e49e8160a39c257e09d8554
JGW-Korea <jgw6372@gmail.com>
2025-08-28 16:48:05 +0900

    comment: Molecular / Filter Group(Sort) 컴포넌트 주석 내용 추가 (#87)

--- src/widgets/filter-group/variant/SortFilterGroup.tsx
+++ src/widgets/filter-group/variant/SortFilterGroup.tsx
@@ -5,6 +5,7 @@ interface Props {
   children: React.ReactElement<typeof Chip> | React.ReactElement<typeof Chip>[];
 }
 
+// 정렬 옵션을 위한 Filter Group 컴포넌트
 export default function SortFilterGroup({ children }: Props) {
   return <View className="flex-row gap-4">{children}</View>;
 }
