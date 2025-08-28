2d21d339594db1d58459217e6bf302103169035d
JGW-Korea <jgw6372@gmail.com>
2025-08-28 16:39:35 +0900

    refactor: Molecular / Filter Group(Label) Chip 버튼 구성 레이아웃 스타일 적용 (#87)

--- src/widgets/filter-group/variant/LabelFilterGroup.tsx
+++ src/widgets/filter-group/variant/LabelFilterGroup.tsx
@@ -10,7 +10,9 @@ export default function LabelFilterGroup({ title, children }: Props) {
   return (
     <View className="gap-1">
       <Text typography="description-md">{title}</Text>
-      {children}
+
+      {/* Filter 버튼 구성 레이아웃 */}
+      <View className="flex-row flex-wrap gap-3 py-2">{children}</View>
     </View>
   );
 }
