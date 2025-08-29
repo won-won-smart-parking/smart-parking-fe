1d11e29672d581620989cf19f3732311405e3ed8
JGW-Korea <jgw6372@gmail.com>
2025-08-29 09:45:24 +0900

    feat: Molecular / Summary Card 기본 파일 및 레이아웃 구성 (#94)

---
+++ src/entities/payment/ui/molecules/summary-card/index.tsx
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+
+export default function PaymentSummaryCard() {
+  return (
+    <View>
+      <View>
+        {/* 일일권 이용내역 요약 개수 */}
+        <PaymentSummaryCard />
+
+        <View />
+
+        {/* 정기권 이용내역 요약 개수 */}
+        <PaymentSummaryCard />
+      </View>
+    </View>
+  );
+}
---
+++ src/entities/payment/ui/molecules/summary-card/part/PaymentSummaryItem.tsx
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import { Icon, Text } from "@shared/ui/atoms";
+
+export default function PaymentSummaryItem() {
+  return (
+    <View>
+      <View>
+        <Icon className="bg-overlay-blue-20" />
+      </View>
+
+      <View>
+        <Text></Text>
+        <Text></Text>
+      </View>
+    </View>
+  );
+}
