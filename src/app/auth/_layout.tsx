e9acd655babc428a608b396c564f130de39664b6
JGW-Korea <jgw6372@gmail.com>
2025-08-29 19:51:45 +0900

    refactor: app/auth/* 네비게이션 Stack -> Slot 네비게이션으로 수정 (#90)

--- src/app/auth/_layout.tsx
+++ src/app/auth/_layout.tsx
@@ -1,27 +1,12 @@
-import { Stack } from "expo-router";
-import { Text } from "@shared/ui/atoms";
+import { Slot } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SectionTitle from "@features/auth/section-title";
 
 export default function AuthLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="sign-in"
-        options={{
-          headerTitle: () => <Text typography="heading-md">로그인을 해주세요.</Text>,
-        }}
-      />
-      {/* <Stack.Screen
-        name="sign-up"
-        options={{
-          headerTitle: () => <Text typography="heading-md">회원탈퇴</Text>,
-        }}
-      /> */}
-      <Stack.Screen
-        name="reset-password"
-        options={{
-          headerTitle: () => <Text typography="heading-md">인증 후 비밀번호를 변경해주세요.</Text>,
-        }}
-      />
-    </Stack>
+    <SafeAreaView className="flex-1">
+      <SectionTitle />
+      <Slot />
+    </SafeAreaView>
   );
 }
--- src/app/auth/sign-in.tsx
+++ src/app/auth/sign-in.tsx
@@ -1,11 +1,16 @@
-import { View } from "react-native";
+import { useRouter } from "expo-router";
+import { Button, View } from "react-native";
 import { Text } from "@shared/ui/atoms";
 
 // smartparking://profile/edit
 export default function SignInScreen() {
+  const router = useRouter();
   return (
     <View className="flex-1 items-center justify-center">
       <Text typography="display-default">로그인 페이지</Text>
+
+      <Button title="회원가입 페이지 이동" onPress={() => router.navigate("/auth/sign-up")} />
+      <Button title="비밀번호 수정 페이지 이동" onPress={() => router.navigate("/auth/reset-password")} />
     </View>
   );
 }
