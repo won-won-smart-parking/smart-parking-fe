2b0c96a773eccf7bf416dee19bf1ba8bc6d8c093
JGW-Korea <jgw6372@gmail.com>
2025-08-29 20:24:19 +0900

    comment: Molecular / Search Field 컴포넌트 설명 주석 작성 (#89)

--- src/shared/ui/molecules/search-field/index.tsx
+++ src/shared/ui/molecules/search-field/index.tsx
@@ -10,6 +10,12 @@ interface Props {
   input: SearchInputProps;
 }
 
+/**
+ * Molecular / Search Field
+ *
+ * 메인 페이지, 검색 페이지에서 공통으로 사용하는 검색 필드 UI 컴포넌트입니다.
+ * 비즈니스 로직은 포함하지 않으며, 상위 컴포넌트에서 필요한 로직과 상태를 주입하여 확장합니다.
+ */
 export default function SearchField({ leftIcon, rightIcon, input }: Props) {
   return (
     <View className="flex-row items-center justify-between gap-2">
