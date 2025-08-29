28d77d694d5e5cf3d03057009c8138d4e24ee773
JGW-Korea <jgw6372@gmail.com>
2025-08-29 16:45:56 +0900

    feat: Molecular / Input Field Button 조건부 렌더링 구성 (#95)

--- src/shared/ui/molecules/input-field/index.tsx
+++ src/shared/ui/molecules/input-field/index.tsx
@@ -1,5 +1,5 @@
-import { Button, View } from "react-native";
-import { Text } from "@shared/ui/atoms";
+import { View } from "react-native";
+import { Button, Text } from "@shared/ui/atoms";
 import { UnderlineInputProps } from "@shared/ui/atoms/input/variant";
 import { HelperText, TextInput } from "./part";
 
@@ -9,9 +9,14 @@ interface Props {
   label: string;
   input: UnderlineInputProps;
   message?: string;
+  button?: {
+    label: string;
+    onPress: () => void;
+    disabled?: boolean;
+  };
 }
 
-export default function InputField({ label, input, message = "" }: Props) {
+export default function InputField({ label, input, message = "", button }: Props) {
   return (
     <View className="gap-2">
       {/* Input Fiedl 제목(Title) */}
@@ -21,9 +26,24 @@ export default function InputField({ label, input, message = "" }: Props) {
 
       {/* Input Field 인풋 + 헬퍼 텍스트 구성 레이아웃 */}
       <View className="gap-1">
-        <View className="flex-row gap-4">
+        <View className="flex-row items-center gap-4">
           <TextInput {...input} />
-          <Button title="Label" />
+          {button ? (
+            <Button
+              variant="label"
+              typography="label-tight"
+              label={button.label}
+              onPress={button.onPress}
+              disabled={input.state === "disabled"}
+              overrideButtonContainerStyles="min-w-24 rounded-[4px]"
+              palette={{
+                bgColor: input.state === "disabled" ? "bg-neutral-500" : "bg-blue-300",
+                bgPressedColor: input.state === "disabled" ? "bg-neutral-600" : "bg-blue-400",
+                textColor: input.state === "disabled" ? "text-neutral-870" : "text-neutral-100",
+                textPressedColor: input.state === "disabled" ? "text-neutral-800" : "text-neutral-300",
+              }}
+            />
+          ) : null}
         </View>
 
         {message ? <HelperText state={input.state} message={message} /> : null}
