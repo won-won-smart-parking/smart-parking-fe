f45e9dad24533b2fd9d67a05cc6686677b03ede1
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-08-29 22:14:23 +0900

    feat: Avatar 컴포넌트 연동 (#86)

--- src/shared/ui/molecules/user-profile/index.tsx
+++ src/shared/ui/molecules/user-profile/index.tsx
@@ -1,4 +1,5 @@
 import { View } from "react-native";
+import Avatar from "@shared/ui/atoms/avatar";
 import Text from "@shared/ui/atoms/text";
 
 interface Props {
@@ -7,9 +8,10 @@ interface Props {
   userEmail: string;
 }
 
-export default function UserProfile({ userName, userEmail }: Props) {
+export default function UserProfile({ userImage, userName, userEmail }: Props) {
   return (
     <View>
+      <Avatar size="md" profileUrl={userImage} />
       <Text typography="title-md">{userName}</Text>
       <Text typography="description-md">{userEmail}</Text>
     </View>
