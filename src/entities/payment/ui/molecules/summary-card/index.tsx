89e4ca5c045a45f9dbb46c64136b51379df836d1
JGW-Korea <jgw6372@gmail.com>
2025-08-29 10:46:41 +0900

    feat: Molecular / Summary Card 컨테이너 스타일 구성 (#94)

--- src/entities/payment/ui/molecules/summary-card/index.tsx
+++ src/entities/payment/ui/molecules/summary-card/index.tsx
@@ -1,16 +1,17 @@
 import { View } from "react-native";
+import PaymentSummaryItem from "./part/PaymentSummaryItem";
 
 export default function PaymentSummaryCard() {
   return (
-    <View>
-      <View>
+    <View className="rounded-[8px] bg-blue-300 px-4 py-5">
+      <View className="flex-row justify-center gap-6">
         {/* 일일권 이용내역 요약 개수 */}
-        <PaymentSummaryCard />
+        <PaymentSummaryItem ticketType="daily" />
 
-        <View />
+        <View className="w-[1px] bg-overlay-white-30" />
 
         {/* 정기권 이용내역 요약 개수 */}
-        <PaymentSummaryCard />
+        <PaymentSummaryItem ticketType="regular" />
       </View>
     </View>
   );
