cf9f79cd7d7fa0340a13baf66a82d2f3a91d3887
JGW-Korea <jgw6372@gmail.com>
2025-08-29 10:45:55 +0900

    feat: Molecular / Summary Card 일일권 / 정기권 표현 UI 스타일 구성 (#94)

--- src/entities/payment/ui/molecules/summary-card/part/PaymentSummaryItem.tsx
+++ src/entities/payment/ui/molecules/summary-card/part/PaymentSummaryItem.tsx
@@ -8,14 +8,16 @@ interface Props {
 
 export default function PaymentSummaryItem({ ticketType, total = 0 }: Props) {
   return (
-    <View>
-      <View className="bg-overlay-blue-20">
+    <View className="flex-row items-center gap-4">
+      <View className="rounded-[8px] bg-overlay-blue-20 p-2">
         <Icon className="w-8 text-neutral-100" name={ticketType === "daily" ? "ticketSaleBroken" : "ticketStar"} />
       </View>
 
-      <View>
-        <Text>{ticketType === "daily" ? "일일권" : "정기권"}</Text>
-        <Text>
+      <View className="gap-1">
+        <Text typography="body-lg" className="text-neutral-100">
+          {ticketType === "daily" ? "일일권" : "정기권"}
+        </Text>
+        <Text typography="title-md" className="text-neutral-100">
           {total}
           {ticketType === "daily" ? "회 이용" : "건 발급"}
         </Text>
