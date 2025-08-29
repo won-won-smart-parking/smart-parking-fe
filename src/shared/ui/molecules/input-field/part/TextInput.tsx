5b26e7837fa5b304cd03e56b1a28525bc39aa305
JGW-Korea <jgw6372@gmail.com>
2025-08-29 13:01:06 +0900

    feat: Molecular / Input Field TextInput 파트 레이아웃 및 Props 구성 (#95)

--- src/shared/ui/molecules/input-field/part/TextInput.tsx
+++ src/shared/ui/molecules/input-field/part/TextInput.tsx
@@ -1,15 +1,6 @@
 import { Input } from "@shared/ui/atoms";
 import { UnderlineInputProps } from "@shared/ui/atoms/input/variant";
 
-export default function TextInput({ state, value, onChangeText, onPress, placeholder }: UnderlineInputProps) {
-  return (
-    <Input
-      variant="underline"
-      state={state}
-      value={value}
-      onChangeText={onChangeText}
-      onPress={onPress}
-      placeholder={placeholder}
-    />
-  );
+export default function TextInput(props: UnderlineInputProps) {
+  return <Input variant="underline" {...props} />;
 }
