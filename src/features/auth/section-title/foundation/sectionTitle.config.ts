5c4a3699697bcc05396f69923671efb314f6fdc4
JGW-Korea <jgw6372@gmail.com>
2025-08-29 19:43:41 +0900

    feat: Molecular / Section Title 각 auth/ 하위 라우트에 맞는 텍스트 구성 옵션 설정 (#90)

---
+++ src/features/auth/section-title/foundation/sectionTitle.config.ts
@@ -0,0 +1,40 @@
+import type { SectionTitleProps } from "./";
+
+export const sectionTitleConfig: { [pathname: string]: SectionTitleProps | ((step: number) => SectionTitleProps) } = {
+  "/auth/sign-in": {
+    title: "로그인을 해주세요.",
+    label: "둘러보기",
+  },
+  "/auth/reset-password": {
+    title: "인증 후 비밀번호 변경해주세요.",
+    label: "취소",
+  },
+  "/auth/sign-up": (step) => {
+    switch (step) {
+      case 1: {
+        return {
+          title: "회원가입을 진행해주세요.",
+          label: "취소",
+        };
+      }
+      case 2: {
+        return {
+          title: "회원가입을 진행해주세요.",
+          label: "취소",
+        };
+      }
+      case 3: {
+        return {
+          title: "회원가입이 완료되었습니다.",
+          label: "",
+        };
+      }
+      default: {
+        return {
+          title: "회원가입",
+          label: "취소",
+        };
+      }
+    }
+  },
+};
