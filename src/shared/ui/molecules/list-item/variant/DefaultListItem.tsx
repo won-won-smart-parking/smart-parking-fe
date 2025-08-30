dd43cd3ecd76dffd99bf0c98d5e9c7c945ae5638
JGW-Korea <jgw6372@gmail.com>
2025-08-30 19:27:35 +0900

    style: Molecular / List Item(default) 제목 + 설명 레이아웃 스타일 수정 (#82)

--- src/shared/ui/molecules/list-item/variant/DefaultListItem.tsx
+++ src/shared/ui/molecules/list-item/variant/DefaultListItem.tsx
@@ -19,7 +19,8 @@ export default function DefaultListItem({ title, description, iconName, directio
         direction === "center" ? "items-center" : "items-start", // 아이콘 정렬을 위한 수직 축에 대한 props를 전달받아 조건부 스타일을 적용합니다.
       )}
     >
-      <View>
+      {/* 리스트의 제목 + 설명을 구성하는 레이아웃 */}
+      <View className="flex-1 gap-1">
         <Text typography="description-md">{title}</Text>
         {description && ( // description은 string \ undefined이기 때문에 값이 없을 경우 출력되지 않는다.
           <Text typography="caption-sm" className="text-neutral-850">
@@ -28,6 +29,7 @@ export default function DefaultListItem({ title, description, iconName, directio
         )}
       </View>
 
+      {/* 아이콘 조건부 렌더링 */}
       {iconName && <Icon name={iconName} />}
     </View>
   );
