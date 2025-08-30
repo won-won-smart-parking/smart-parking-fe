3e923ab312cc87cac2ee0933f445f9dab0b754a4
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-08-30 17:36:38 +0900

    refactor: stylesheet 삭제 및 수정 (#88)

--- src/shared/ui/molecules/marker/index.tsx
+++ src/shared/ui/molecules/marker/index.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import Dot from "@entities/ui/atoms/dot";
 import { ParkingStatus } from "@shared/types/parking-status";
 import { Icon } from "@shared/ui/atoms";
@@ -7,46 +7,33 @@ import Text from "@shared/ui/atoms/text";
 
 interface Props {
   ticketPrice: string;
-  active: boolean;
+  selected: boolean;
   state: ParkingStatus;
 }
 
-// 마커 장소 표시 부분
-const Oval = () => <View style={styles.oval} />;
-
-const styles = StyleSheet.create({
-  oval: {
-    width: 12,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: "rgba(0,0,0,0.08)",
-    transform: [{ scaleX: 2 }],
-  },
-});
-
-export default function Marker({ ticketPrice, active, state }: Props) {
+export default function Marker({ ticketPrice, selected, state }: Props) {
   return (
     <View className="items-center gap-1">
       {/* 마커 정보 표시 부분 */}
       <View
         className={clsx(
           "relative flex-row items-center gap-1 rounded-full border px-2 py-1",
-          active ? "border-blue-300 bg-blue-300" : "border-overlay-black-08 bg-neutral-100",
+          selected ? "border-blue-300 bg-blue-300" : "border-overlay-black-08 bg-neutral-100",
         )}
       >
         {/* 주차 상태 Dot */}
-        <View style={{ position: "absolute", left: 68, top: -30, bottom: 0, justifyContent: "center" }}>
+        <View className="absolute bottom-6.5 right-3">
           <Dot status={state} />
         </View>
         {/* 아이콘 */}
-        <Icon name="parking" className={clsx("h-4 w-4", active ? "text-neutral-100" : "text-blue-300")} />
+        <Icon name="parking" className={clsx("w-4", selected ? "text-neutral-100" : "text-blue-300")} />
 
         {/* 가격 */}
-        <Text typography="description-md" className={clsx(active ? "text-neutral-100" : "text-neutral-1000")}>
+        <Text typography="description-md" className={clsx(selected ? "text-neutral-100" : "text-neutral-1000")}>
           {ticketPrice}
         </Text>
       </View>
-      <Oval />
+      <View className="h-2 w-3 scale-x-[2] rounded-full bg-overlay-black-08" />
     </View>
   );
 }
