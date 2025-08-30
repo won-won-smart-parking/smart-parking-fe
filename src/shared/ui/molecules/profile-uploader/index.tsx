ec9f9ca9ec77efc35d90ad6db64580c4ebed8913
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-08-30 16:01:50 +0900

    docs: 주석 추가 (#96)

--- src/shared/ui/molecules/profile-uploader/index.tsx
+++ src/shared/ui/molecules/profile-uploader/index.tsx
@@ -1,6 +1,6 @@
 import { Pressable, View } from "react-native";
-import { Icon } from "@shared/ui/atoms";
 import Avatar from "@shared/ui/atoms/avatar";
+import Icon from "@shared/ui/atoms/icon";
 
 interface Props {
   profileUrl?: string;
@@ -12,6 +12,8 @@ export default function ProfileUploader({ profileUrl }: Props) {
       <Pressable>
         <Avatar size="lg" profileUrl={profileUrl} />
       </Pressable>
+
+      {/* 업로드 버튼 */}
       <Pressable
         className="h-8 w-8 items-center justify-center rounded-full bg-blue-300"
         style={{ position: "absolute", right: 3, bottom: 0 }}
