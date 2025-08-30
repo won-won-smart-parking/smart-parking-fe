18e4d5ad4296510a6dd48c30444456d13fe5e71c
JGW-Korea <jgw6372@gmail.com>
2025-08-30 19:21:29 +0900

    feat: Molecular / List Item(default) Props 구성 및 스타일 적용 (#82)

--- src/shared/ui/molecules/list-item/variant/DefaultListItem.tsx
+++ src/shared/ui/molecules/list-item/variant/DefaultListItem.tsx
@@ -1,22 +1,28 @@
+import clsx from "clsx";
 import { View } from "react-native";
 import { Icon, Text } from "@shared/ui/atoms";
 import { IconName } from "@shared/ui/atoms/icon/variant";
 
-interface Props {
+export interface Props {
   title: string;
+  direction?: "top" | "center"; // 리터럴 타입 유니온으로 구성
   description?: string;
   iconName?: IconName;
 }
 
-export default function DefaultListItem({ title, description, iconName }: Props) {
+export default function DefaultListItem({ title, description, iconName, direction = "center" }: Props) {
   return (
-    <View>
+    <View className={clsx("flex-row justify-between", direction === "center" ? "items-center" : "items-start")}>
       <View>
-        <Text>{title}</Text>
-        {description ? <Text>{description}</Text> : null}
+        <Text typography="description-md">{title}</Text>
+        {description && ( // description은 string \ undefined이기 때문에 값이 없을 경우 출력되지 않는다.
+          <Text typography="caption-sm" className="text-neutral-850">
+            {description}
+          </Text>
+        )}
       </View>
 
-      {iconName ? <Icon name={iconName} /> : null}
+      {iconName && <Icon name={iconName} />}
     </View>
   );
 }
