95ce831be34d954780cdf7388fe4b4d31cbc7aa5
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-08-30 17:03:31 +0900

    refactor: props 수정 및 단일 책임 컴포넌트로 단순화 (#86)

--- src/shared/ui/molecules/user-profile/index.tsx
+++ src/shared/ui/molecules/user-profile/index.tsx
@@ -3,23 +3,12 @@ import Avatar from "@shared/ui/atoms/avatar";
 import Text from "@shared/ui/atoms/text";
 
 interface Props {
-  isLogIn: boolean;
   userImage?: string;
-  userName?: string;
-  userEmail?: string;
+  userName: string;
+  userEmail: string;
 }
 
-export default function UserProfile({ isLogIn, userImage, userName, userEmail }: Props) {
-  if (!isLogIn) {
-    return (
-      <View className="items-center">
-        <Text typography="caption-xxl" className="text-neutral-1000">
-          로그인 후 다양한 기능을 이용해 보세요
-        </Text>
-      </View>
-    );
-  }
-
+export default function UserProfile({ userImage, userName, userEmail }: Props) {
   return (
     <View className="flex-row items-center justify-center gap-6">
       <Avatar size="md" profileUrl={userImage} />
