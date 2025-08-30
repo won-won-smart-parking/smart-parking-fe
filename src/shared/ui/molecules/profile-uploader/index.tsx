1637e31aa88cd073bf9b935e48c5ed34230ecd91
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-08-30 16:01:07 +0900

    feat: 업로드 버튼 추가 및 위치 조정 (#96)

--- src/shared/ui/molecules/profile-uploader/index.tsx
+++ src/shared/ui/molecules/profile-uploader/index.tsx
@@ -1,4 +1,5 @@
-import { Pressable } from "react-native";
+import { Pressable, View } from "react-native";
+import { Icon } from "@shared/ui/atoms";
 import Avatar from "@shared/ui/atoms/avatar";
 
 interface Props {
@@ -7,8 +8,16 @@ interface Props {
 
 export default function ProfileUploader({ profileUrl }: Props) {
   return (
-    <Pressable>
-      <Avatar size="lg" profileUrl={profileUrl} />
-    </Pressable>
+    <View className="relative">
+      <Pressable>
+        <Avatar size="lg" profileUrl={profileUrl} />
+      </Pressable>
+      <Pressable
+        className="h-8 w-8 items-center justify-center rounded-full bg-blue-300"
+        style={{ position: "absolute", right: 3, bottom: 0 }}
+      >
+        <Icon name="plus" className="text-neutral-100" />
+      </Pressable>
+    </View>
   );
 }
