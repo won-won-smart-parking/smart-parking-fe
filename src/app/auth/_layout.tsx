ebba76f0bc60d9c2189884ee3676f122779ff2d5
JGW-Korea <jgw6372@gmail.com>
2025-09-02 19:08:49 +0900

    move: 모든 컴포넌트 src/shared/ui로 임시 이동 (#117)

--- src/app/auth/_layout.tsx
+++ src/app/auth/_layout.tsx
@@ -1,11 +1,12 @@
 import { Slot } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import SectionTitle from "@features/auth/section-title";
+
+// import SectionTitle from "@features/auth/section-title";
 
 export default function AuthLayout() {
   return (
     <SafeAreaView className="flex-1">
-      <SectionTitle />
+      {/* <SectionTitle /> */}
       <Slot />
     </SafeAreaView>
   );
