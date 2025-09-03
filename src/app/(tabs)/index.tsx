f39357db706a221be04fbe2f0f4bd7e31b242ee3
JGW-Korea <jgw6372@gmail.com>
2025-09-03 19:45:29 +0900

    test: TanStack + Axios 연동 확인 (#117)

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,23 @@
+import axios from "axios";
 import { View } from "react-native";
 import { Text } from "@shared/ui/atoms";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchUser = async () => {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+  return response;
+};
 
 // smartparking://
 export default function MainScreen() {
+  const { isLoading, isError } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUser,
+  });
+
+  if (isLoading) return <Text>Lodaing...</Text>;
+  if (isError) return <Text>Error...</Text>;
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text typography="display-default">메인 페이지</Text>
