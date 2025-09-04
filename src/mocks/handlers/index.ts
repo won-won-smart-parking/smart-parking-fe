eb840fcdc71b0103874fea9127af3a1d5480eaff
JGW-Korea <jgw6372@gmail.com>
2025-09-04 14:36:16 +0900

    fix: RN 환경 호스트 차이를 고려한 핸들러 URL 호스트 무시 경로로 변경 (#117)

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -2,7 +2,7 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.get("/user", () => {
+  http.get("*/user", () => {
     return HttpResponse.json({ name: "John Maverick" });
   }),
 ];
