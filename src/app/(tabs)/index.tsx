ac9b484ab3eedd8af2e3c4921f7dcb18821488de
JGW-Korea <jgw6372@gmail.com>
2025-09-04 14:37:20 +0900

    test: Mocking Service Worker(MSW) 연동 확인 테스트 (#117)

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
-import axios from "axios";
+// import axios from "axios";
+// import { useEffect } from "react";
 import { View } from "react-native";
 import { create } from "zustand";
 import { Text } from "@shared/ui/atoms";
-import { useQuery } from "@tanstack/react-query";
 
-const fetchUser = async () => {
-  const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-  return response;
-};
+// import { useQuery } from "@tanstack/react-query";
+
+// const fetchUser = async () => {
+//   const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+//   return response;
+// };
 
 interface ZustandStateProps {
   count: number;
@@ -29,17 +31,38 @@ const useZustandStore = create<ZustandStateProps>((set, get) => {
   };
 });
 
+// const instance = axios.create({
+//   adapter: "fetch",
+//   baseURL: "",
+// });
+
 // smartparking://
 export default function MainScreen() {
   const count = useZustandStore((state) => state.count);
 
-  const { isLoading, isError } = useQuery({
-    queryKey: ["users"],
-    queryFn: fetchUser,
-  });
+  // const { isLoading, isError } = useQuery({
+  //   queryKey: ["users"],
+  //   queryFn: fetchUser,
+  // });
 
-  if (isLoading) return <Text>Lodaing...</Text>;
-  if (isError) return <Text>Error...</Text>;
+  // useEffect(() => {
+  //   async function fetchMockUser() {
+  //     try {
+  //       const response = await instance.get("/user");
+  //       console.log(response);
+  //       if (response.status === 200) {
+  //         throw new Error("무언가 잘못되었습니다.", { cause: "asd" });
+  //       }
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   }
+
+  //   fetchMockUser();
+  // }, []);
+
+  // if (isLoading) return <Text>Lodaing...</Text>;
+  // if (isError) return <Text>Error...</Text>;
 
   return (
     <View className="flex-1 items-center justify-center">
