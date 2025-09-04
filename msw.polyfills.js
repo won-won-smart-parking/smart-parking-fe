82fdb3109cd93abbb37180ec851986ddce5c393b
JGW-Korea <jgw6372@gmail.com>
2025-09-04 14:24:28 +0900

    fix: MSW polyfills MessageEvent 에러 해결 (#117)

--- msw.polyfills.js
+++ msw.polyfills.js
@@ -1,2 +1,17 @@
 import "fast-text-encoding";
 import "react-native-url-polyfill/auto";
+
+// ReferenceError: Property 'MessageEvent' doesn't exist, js engine: hermes 해결 방법
+// [관련 GitHub 이슈에서 제안된 해결 방법](https://github.com/mswjs/mswjs.io/issues/453)
+function defineMockGlobal(name) {
+  if (typeof global[name] === "undefined") {
+    global[name] = class {
+      constructor(type, eventInitDict) {
+        this.type = type;
+        Object.assign(this, eventInitDict);
+      }
+    };
+  }
+}
+
+["MessageEvent", "Event", "EventTarget", "BroadcastChannel"].forEach(defineMockGlobal);
