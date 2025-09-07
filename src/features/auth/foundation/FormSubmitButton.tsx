73874e3e4734f686190ffc0045e67071d01692b3
JGW-Korea <jgw6372@gmail.com>
2025-09-08 04:12:03 +0900

    feat: Organism / Form 폼 제출 버튼 컴포넌트 기본 레이아웃 구성 (#124)

---
+++ src/features/auth/foundation/FormSubmitButton.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@shared/ui/atoms";
+
+interface Props {
+  label: string;
+  disabled: boolean;
+}
+
+export default function FormSubmitButton({ label, disabled, ...rest }: Props) {
+  return (
+    <Button
+      variant="label"
+      label={label}
+      disabled={disabled}
+      palette={
+        disabled
+          ? {
+              bgColor: "bg-neutral-500",
+              bgPressedColor: "bg-neutral-500",
+              textColor: "text-neutral-870",
+              textPressedColor: "text-neutral-870",
+            }
+          : {
+              bgColor: "bg-blue-300",
+              bgPressedColor: "bg-blue-300",
+              textColor: "text-neutral-100",
+              textPressedColor: "text-neutral-100",
+            }
+      }
+      disablePressedEffect
+      fullWidth
+      {...rest}
+    />
+  );
+}
