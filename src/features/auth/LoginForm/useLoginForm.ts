ce001c61957b60a820cf6d71420b7a0bc9d223be
JGW-Korea <jgw6372@gmail.com>
2025-09-08 16:25:04 +0900

    feat: Organism / Form(login) 제출 이벤트 핸들러 구성 및 refs 객체 구성 (#124)

--- src/features/auth/LoginForm/useLoginForm.ts
+++ src/features/auth/LoginForm/useLoginForm.ts
@@ -1,5 +1,8 @@
-import { useReducer } from "react";
+import React, { useReducer, useRef } from "react";
+import { TextInput } from "react-native";
+import { InputState } from "@shared/ui/atoms/input/foundation";
 import { Action, FormField, LoginFormType } from "./loginFormType";
+import { validateLoginFormField } from "../utils/validate";
 
 // 상태 객체 내부 업데이트 로직
 function patchField(state: LoginFormType, name: keyof LoginFormType, patch: Partial<FormField>) {
@@ -12,17 +15,20 @@ function reducer(state: LoginFormType, action: Action) {
 
   // action.type에 맞는 상태 업데이트 로직을 수행한다.
   switch (type) {
-    case "CHANG_VALUE": {
+    case "CHANGE_VALUE": {
       return patchField(state, payload.name, { value: payload.text });
     }
     case "CLEAR": {
       return patchField(state, payload.name, { value: "" });
     }
     case "FOCUS": {
-      return patchField(state, payload.name, { inputState: "focus" });
+      return patchField(state, payload.name, { inputState: payload.inputState !== "error" ? "focus" : "error" });
     }
     case "BLUR": {
       return patchField(state, payload.name, { inputState: "default" });
+    }
+    case "ERROR": {
+      return patchField(state, payload.name, { inputState: "error" });
     }
   }
 }
@@ -35,13 +41,46 @@ const initState: LoginFormType = {
 
 // 로그인 폼에서 사용될 사용자 정의 훅(Custom Hook)
 export default function useLoginForm() {
-  const [state, dispatch] = useReducer(reducer, initState); // useState로 각 상태 업데이트 로직을 관리하는 것보다 useReducer 방식이 훨씬 가독성이 높다.
+  const [state, dispatch] = useReducer(reducer, initState); // useState로 각 상태 업데이트 로직을 관리하는 것보다 useReducer 관리 방식이 분기 처리가 하기가 더 낫다.
+
+  const refs = useRef<Record<keyof LoginFormType, React.RefObject<TextInput | null>>>({
+    id: { current: null },
+    password: { current: null },
+  });
 
   // InputField 컴포넌트에 전달할 이벤트 핸들러
-  const handleChangeValue = (text: string, name: keyof LoginFormType) => dispatch({ type: "CHANG_VALUE", payload: { name, text } });
+  const handleChangeValue = (text: string, name: keyof LoginFormType) => dispatch({ type: "CHANGE_VALUE", payload: { name, text } });
   const handleClearPress = (name: keyof LoginFormType) => dispatch({ type: "CLEAR", payload: { name } });
-  const handleFocus = (name: keyof LoginFormType) => dispatch({ type: "FOCUS", payload: { name } });
+  const handleFocus = (name: keyof LoginFormType, inputState: InputState) => dispatch({ type: "FOCUS", payload: { name, inputState } });
   const handleBlur = (name: keyof LoginFormType) => dispatch({ type: "BLUR", payload: { name } });
 
-  return { state, handleChangeValue, handleClearPress, handleFocus, handleBlur };
+  // Submit 이벤트 핸들러
+  const handleSubmit = () => {
+    const errors: (keyof LoginFormType)[] = [];
+
+    // 로그인 제출 버튼 클릭 시 발생한 에러 항목 누적
+    (Object.keys(state) as (keyof LoginFormType)[]).forEach((key) => {
+      const errorMsg = validateLoginFormField(key, state[key].value);
+
+      // 단일 인풋 필드 검증 오류 메시지가 있을 경우 각 키를 누적한다.
+      if (errorMsg) {
+        errors.push(key);
+      }
+    });
+
+    // 발생한 잘못된 검증이 발생한 경우 각 인풋 필드를 에러 처리 및 포커싱을 수행한다.
+    if (errors.length > 0) {
+      const firstError = errors[0];
+      if (refs.current[firstError].current) {
+        refs.current[firstError].current.focus();
+      }
+
+      errors.forEach((name) => dispatch({ type: "ERROR", payload: { name } }));
+      return; // 에러가 발생하면 제출 로직이 수행되면 안됨
+    }
+
+    console.log("이상 없음!!");
+  };
+
+  return { state, refs, handleSubmit, handleChangeValue, handleClearPress, handleFocus, handleBlur };
 }
