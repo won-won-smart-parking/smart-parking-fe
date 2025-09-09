ba9e10d6d55c94969c77847039be622175304382
JGW-Korea <jgw6372@gmail.com>
2025-09-09 19:47:42 +0900

    refactor: Organism / Form(login) 커스텀 훅 내부 코드 수정 (#124)

--- src/features/auth/LoginForm/loginFormType.ts
+++ src/features/auth/LoginForm/loginFormType.ts
@@ -1,3 +1,4 @@
+import { IconName } from "@shared/ui/atoms/icon/variant";
 import { InputState } from "@shared/ui/atoms/input/foundation";
 
 type LoginFieldName = "id" | "password"; // 로그인 폼에서 사용되는 필드 키 이름
@@ -6,7 +7,42 @@ type LoginFieldName = "id" | "password"; // 로그인 폼에서 사용되는 필
 export interface FormField {
   value: string;
   inputState: InputState;
+  icon?: {
+    revealed: boolean;
+    hidden: IconName;
+    visible: IconName;
+  };
+  message: string;
+  button?: {
+    label: string;
+    onPress: () => void;
+    disabled: boolean;
+  };
 }
+
+/*
+  InputFieldProps
+  - InputField - Props 구성
+      - input: {
+          **Underline - Props 구성**
+          - InputRequiredProps
+            - value
+          
+            - state: InputState("default" | "focus" | "error" | "disabled")
+          
+          - icon?:
+            - revealed: 아이콘 hidden, visiable 상태 여부
+            - hidden: 숨겨진 상태에서의 아이콘 이름
+            - visiable: 보여짐 상태에서의 아이콘 이름
+            - onPress: 아이콘 클릭 시 변경 상태
+      }
+      - message?: HelperText
+      - button?: {
+        - label -> 버튼의 텍스트 내용
+        - onPress -> 버튼 클릭 시 발생하는 press 이벤트 핸들러
+        - disabled -> 버튼 비활성화 여부	
+      }
+*/
 
 // 로그인 폼 전체 상태 구조
 export type LoginFormType = {
@@ -19,7 +55,8 @@ type ActionMap = {
   CLEAR: { name: LoginFieldName };
   FOCUS: { name: LoginFieldName; inputState: InputState };
   BLUR: { name: LoginFieldName };
-  ERROR: { name: LoginFieldName };
+  ERROR: { name: LoginFieldName; message: string };
+  TOGGLE_REVEAL: { name: LoginFieldName };
 };
 
 export type Action = { [K in keyof ActionMap]: { type: K; payload: ActionMap[K] } }[keyof ActionMap];
--- src/features/auth/LoginForm/useLoginForm.ts
+++ src/features/auth/LoginForm/useLoginForm.ts
@@ -28,20 +28,35 @@ function reducer(state: LoginFormType, action: Action) {
       return patchField(state, payload.name, { inputState: "default" });
     }
     case "ERROR": {
-      return patchField(state, payload.name, { inputState: "error" });
+      return patchField(state, payload.name, { inputState: "error", message: payload.message });
+    }
+    case "TOGGLE_REVEAL": {
+      if (payload.name === "password" && state["password"].icon) {
+        return patchField(state, "password", { icon: { ...state["password"].icon, revealed: !state["password"].icon.revealed } });
+      }
+      return state;
+    }
+    default: {
+      return state;
     }
   }
 }
 
 // useReducer 두 번째 인수로 들어가는 초기 상태 값
 const initState: LoginFormType = {
-  id: { value: "", inputState: "default" },
-  password: { value: "", inputState: "default" },
+  id: { value: "", inputState: "default", message: "" },
+  password: { value: "", inputState: "default", message: "", icon: { revealed: false, hidden: "eyeOffOutline", visible: "eyeOnOutline" } },
 };
+
+// message <- 인풋 필드가 무엇인지 도와주는 Helper Message
+// button  <- 인풋 필드의 부가 효과가 부여하기 위한 버튼의 구성 (인증번호 전송, 핸드폰 번호 인증, ... 등)
+// Login
+// id        -> value, inputState
+// password  -> value, inputState, icon
 
 // 로그인 폼에서 사용될 사용자 정의 훅(Custom Hook)
 export default function useLoginForm() {
-  const [state, dispatch] = useReducer(reducer, initState); // useState로 각 상태 업데이트 로직을 관리하는 것보다 useReducer 관리 방식이 분기 처리가 하기가 더 낫다.
+  const [loginInputState, dispatch] = useReducer(reducer, initState); // useState로 각 상태 업데이트 로직을 관리하는 것보다 useReducer 관리 방식이 분기 처리가 하기가 더 낫다.
 
   const refs = useRef<Record<keyof LoginFormType, React.RefObject<TextInput | null>>>({
     id: { current: null },
@@ -53,34 +68,35 @@ export default function useLoginForm() {
   const handleClearPress = (name: keyof LoginFormType) => dispatch({ type: "CLEAR", payload: { name } });
   const handleFocus = (name: keyof LoginFormType, inputState: InputState) => dispatch({ type: "FOCUS", payload: { name, inputState } });
   const handleBlur = (name: keyof LoginFormType) => dispatch({ type: "BLUR", payload: { name } });
+  const handleRevealToggle = (name: keyof LoginFormType) => dispatch({ type: "TOGGLE_REVEAL", payload: { name } });
 
   // Submit 이벤트 핸들러
   const handleSubmit = () => {
-    const errors: (keyof LoginFormType)[] = [];
+    const errors: [keyof LoginFormType, string][] = [];
 
     // 로그인 제출 버튼 클릭 시 발생한 에러 항목 누적
-    (Object.keys(state) as (keyof LoginFormType)[]).forEach((key) => {
-      const errorMsg = validateLoginFormField(key, state[key].value);
+    (Object.keys(loginInputState) as (keyof LoginFormType)[]).forEach((key) => {
+      const errorMsg = validateLoginFormField(key, loginInputState[key].value);
 
       // 단일 인풋 필드 검증 오류 메시지가 있을 경우 각 키를 누적한다.
       if (errorMsg) {
-        errors.push(key);
+        errors.push([key, errorMsg]);
       }
     });
 
     // 발생한 잘못된 검증이 발생한 경우 각 인풋 필드를 에러 처리 및 포커싱을 수행한다.
     if (errors.length > 0) {
-      const firstError = errors[0];
+      const [firstError, _] = errors[0];
       if (refs.current[firstError].current) {
         refs.current[firstError].current.focus();
       }
 
-      errors.forEach((name) => dispatch({ type: "ERROR", payload: { name } }));
+      errors.forEach(([name, errorMsg]) => dispatch({ type: "ERROR", payload: { name, message: errorMsg } }));
       return; // 에러가 발생하면 제출 로직이 수행되면 안됨
     }
 
     console.log("이상 없음!!");
   };
 
-  return { state, refs, handleSubmit, handleChangeValue, handleClearPress, handleFocus, handleBlur };
+  return { loginInputState, refs, handleSubmit, handleChangeValue, handleClearPress, handleFocus, handleBlur, handleRevealToggle };
 }
