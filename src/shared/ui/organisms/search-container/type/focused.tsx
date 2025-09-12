ac25cda3af6fd9ac287716f494076e77c59bf619
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-09-12 23:11:50 +0900

    feat: FocusedSearchContainer 입력 상태 관리 로직 추가 (#127)

--- src/shared/ui/organisms/search-container/type/focused.tsx
+++ src/shared/ui/organisms/search-container/type/focused.tsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import { View } from "react-native";
 import SearchField from "@shared/ui/molecules/search-field";
 
 export default function FocusedSearchContainer() {
+  const [value, setValue] = useState("");
+
   return (
     <View className="border-b border-neutral-400 px-3 py-2">
       <SearchField
         leftIcon={{
           iconName: "arrowLeft",
-          onPress: () => {},
+          onPress: () => {
+            setValue("");
+          },
         }}
         input={{
           placeholder: "",
-          value: "",
-          onChangeText: () => {},
+          value,
+          onChangeText: setValue,
           onPress: () => {},
         }}
         rightIcon={{ iconName: "mic", onPress: () => {} }}
