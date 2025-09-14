2d763564348387d03af4c8ca3d3b071949eaa059
JGW-Korea <jgw6372@gmail.com>
2025-09-14 16:15:59 +0900

    test: Custom Drawer View 구성 (#126)

--- src/app/(tabs)/(home)/_layout.tsx
+++ src/app/(tabs)/(home)/_layout.tsx
@@ -1,8 +1,22 @@
 import { Drawer } from "expo-router/drawer";
+import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
+
+// screenOptions={{ headerShown: false }}
+
+function SideMenu() {
+  return (
+    <DrawerContentScrollView>
+      <DrawerItem label="Navigate..." />
+      <DrawerItem label="Navigate..." />
+      <DrawerItem label="Navigate..." />
+      <DrawerItem label="Navigate..." />
+    </DrawerContentScrollView>
+  );
+}
 
 export default function HomeDrawerLayout() {
   return (
-    <Drawer>
+    <Drawer screenOptions={{ swipeEnabled: false, drawerType: "front" }} drawerContent={() => <SideMenu />}>
       <Drawer.Screen name="index" />
     </Drawer>
   );
