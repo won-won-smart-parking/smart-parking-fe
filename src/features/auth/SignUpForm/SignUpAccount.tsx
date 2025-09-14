583da23c2c1399b2cce36cc16a1b9e6479f0607a
JGW-Korea <jgw6372@gmail.com>
2025-09-14 23:34:28 +0900

    feat: Organism / Form(signup) 각 스텝 컴포넌트 파일 구성 (#124)

---
+++ src/features/auth/SignUpForm/SignUpAccount.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "@shared/ui/atoms";
+
+export default function SignUpAccount() {
+  return (
+    <View>
+      <Text>Sign Up Account</Text>
+    </View>
+  );
+}
---
+++ src/features/auth/SignUpForm/SignUpVerification.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "@shared/ui/atoms";
+
+export default function SignUpVerification() {
+  return (
+    <View>
+      <Text>Sign Up Verification</Text>
+    </View>
+  );
+}
