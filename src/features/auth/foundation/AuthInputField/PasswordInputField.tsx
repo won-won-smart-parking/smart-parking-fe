64550ca701740e54a299202c9a2c9a1a2e8a9574
JGW-Korea <jgw6372@gmail.com>
2025-09-15 07:52:06 +0900

    feat: 비밀번호 입력 필드 및 유효성 검사 규칙 관리 컴포넌트 구성 (#124)

---
+++ src/features/auth/foundation/AuthInputField/PasswordInputField.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { SignUpFormValues } from "@/process/signup";
+import FormInputController, { Props as FormInputControllerProps } from "../FormInputController";
+
+type Props = Omit<FormInputControllerProps<SignUpFormValues, "account.password">, "inputField" | "rules">;
+
+// 비밀번호 입력 필드 및 유효성 검사 규칙 관리 컴포넌트
+export default function PasswordInputField(props: Props) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <FormInputController<SignUpFormValues, "account.password">
+      {...props}
+      rules={{
+        required: { value: true, message: "필수 입력 항목입니다." },
+        minLength: { value: 8, message: "비밀번호 길이는 최소 8자 이상입니다." },
+        pattern: { value: new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$"), message: "올바른 비밀번호 형식이 아닙니다." },
+      }}
+      inputField={{
+        title: "비밀번호",
+        input: {
+          placeholder: "･････････",
+          secureTextEntry: !showPassword,
+          icon: {
+            hidden: "eyeOffOutline",
+            visible: "eyeOnOutline",
+            onPress: () => setShowPassword((show) => !show),
+          },
+        },
+      }}
+    />
+  );
+}
