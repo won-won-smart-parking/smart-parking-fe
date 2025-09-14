0a1522d60b4b33f05bd579721e90f13fedfc8cf6
JGW-Korea <jgw6372@gmail.com>
2025-09-15 07:51:41 +0900

    feat: 이름 입력 필드 및 유효성 검사 규칙 관리 컴포넌트 구성 (#124)

---
+++ src/features/auth/foundation/AuthInputField/NameInputField.tsx
@@ -0,0 +1,20 @@
+import { SignUpFormValues } from "@/process/signup";
+import FormInputController, { Props as FormInputControllerProps } from "../FormInputController";
+
+type Props = Omit<FormInputControllerProps<SignUpFormValues, "account.name">, "inputField" | "rules">;
+
+// 이름 입력 필드 및 유효성 검사 규칙 관리 컴포넌트
+export default function NameInputField(props: Props) {
+  return (
+    <FormInputController<SignUpFormValues, "account.name">
+      {...props}
+      rules={{ required: { value: true, message: "필수 입력 항목입니다." } }}
+      inputField={{
+        title: "이름",
+        input: {
+          placeholder: "이름을 입력해주세요.",
+        },
+      }}
+    />
+  );
+}
