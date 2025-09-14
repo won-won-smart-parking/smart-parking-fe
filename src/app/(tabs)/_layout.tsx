5675db07371587b62ca25ffa1fd2d1e962ced599
JGW-Korea <jgw6372@gmail.com>
2025-09-14 21:49:06 +0900

    refactor: RootLayout + (tabs) 라우팅 구조 수정 (#126)

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ export default function TabLayout() {
   return (
     <Tabs screenOptions={{ headerTitleAlign: "center" }}>
       <Tabs.Screen
-        name="(home)"
+        name="index"
         options={{
           headerShown: false,
           tabBarLabel: ({ focused }: { focused: boolean }) => (
