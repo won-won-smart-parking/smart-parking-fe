a7843a263084959d05e3d036537decaba689cbcf
JGW-Korea <jgw6372@gmail.com>
2025-09-14 17:26:59 +0900

    feat: Organism / Side-menu 유저 프로필 + 아바타 스타일 수정 (#126)

--- src/app/(tabs)/(home)/_layout.tsx
+++ src/app/(tabs)/(home)/_layout.tsx
@@ -1,15 +1,47 @@
 import { Drawer } from "expo-router/drawer";
-import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
+import { useState } from "react";
+import { Pressable, View } from "react-native";
+import { DrawerContentScrollView } from "@react-navigation/drawer";
+import { Avatar, Icon, Text } from "@shared/ui/atoms";
 
 // screenOptions={{ headerShown: false }}
 
 function SideMenu() {
+  const [isLogin, _] = useState(true);
+
   return (
-    <DrawerContentScrollView>
-      <DrawerItem label="Navigate..." />
-      <DrawerItem label="Navigate..." />
-      <DrawerItem label="Navigate..." />
-      <DrawerItem label="Navigate..." />
+    <DrawerContentScrollView contentContainerStyle={{ flex: 1, justifyContent: "space-between" }}>
+      <View>
+        {/* 로그인(마이페이지 이동) / 비로그인(로그인 페이지 이동) */}
+        <View className="border-b border-coolgray-200 py-4">
+          <Pressable>
+            {/* 로그인 + 비로그인 처리 */}
+            {!isLogin ? (
+              <View className="flex-row items-center gap-0.5">
+                <Text typography="body-xl" className="text-neutral-1000">
+                  로그인
+                </Text>
+                <Icon name="arrowRight" className="w-3 text-neutral-1000" />
+              </View>
+            ) : (
+              <View className="flex-row gap-3">
+                <Avatar size="sm" />
+                <View className="flex-row items-center gap-0.5">
+                  <Text typography="body-xl" className="text-neutral-1000">
+                    로그인
+                  </Text>
+                  <Icon name="arrowRight" className="w-3 text-neutral-1000" />
+                </View>
+              </View>
+            )}
+          </Pressable>
+        </View>
+
+        {/* 네비게이션 */}
+        {/* 차량 관리 + 내 주차권 + 즐겨찾기 */}
+        {/* 서비스 정보(공지사항 + 환경 설정) */}
+        <View></View>
+      </View>
     </DrawerContentScrollView>
   );
 }
