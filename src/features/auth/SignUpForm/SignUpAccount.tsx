052244380d88c6de9ee382467b1ed1fa5ad57cde
JGW-Korea <jgw6372@gmail.com>
2025-09-17 19:40:22 +0900

    feat: Organism / Form(signup) 계정 정보 관리 폼 컴포넌트 전체 구성 (#124)

--- src/features/auth/SignUpForm/SignUpAccount.tsx
+++ src/features/auth/SignUpForm/SignUpAccount.tsx
@@ -2,34 +2,43 @@ import { useFormContext } from "react-hook-form";
 import { View } from "react-native";
 import { SignUpFormValues } from "@/process/signup";
 import { EmailInputField, NameInputField, PasswordConfirmInputField, PasswordInputField } from "../foundation";
+import LocationAgreement from "../foundation/AuthInputField/LocationAgreement";
+import PushNoticeAgreement from "../foundation/AuthInputField/PushNoticeAgreement";
 
 export default function SignUpAccount() {
   const { control, resetField } = useFormContext<SignUpFormValues>();
 
   return (
-    <View>
-      <NameInputField control={{ name: "account.name", control }} resetField={resetField} />
-      <EmailInputField
-        control={{
-          name: "account.email",
-          control,
-        }}
-        resetField={resetField}
-      />
-      <PasswordInputField
-        control={{
-          name: "account.password",
-          control,
-        }}
-        resetField={resetField}
-      />
-      <PasswordConfirmInputField
-        control={{
-          name: "account.passwordConfirm",
-          control,
-        }}
-        resetField={resetField}
-      />
-    </View>
+    <>
+      <View className="gap-4">
+        <NameInputField control={{ name: "account.name", control }} resetField={resetField} />
+        <EmailInputField
+          control={{
+            name: "account.email",
+            control,
+          }}
+          resetField={resetField}
+        />
+        <PasswordInputField
+          control={{
+            name: "account.password",
+            control,
+          }}
+          resetField={resetField}
+        />
+        <PasswordConfirmInputField
+          control={{
+            name: "account.passwordConfirm",
+            control,
+          }}
+          resetField={resetField}
+        />
+      </View>
+
+      <View className="gap-5">
+        <LocationAgreement control={{ name: "account.agreeLocation", control }} />
+        <PushNoticeAgreement control={{ name: "account.agreePushNotice", control }} />
+      </View>
+    </>
   );
 }
