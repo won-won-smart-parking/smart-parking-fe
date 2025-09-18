403845e201d5597e52c81bc6f4752b7bf960c6db
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2025-09-18 11:06:51 +0900

    feat: title Type 추가 및 스타일 정의 (#125)

--- src/shared/ui/organisms/list-group/index.tsx
+++ src/shared/ui/organisms/list-group/index.tsx
@@ -1,16 +1,37 @@
 import { View } from "react-native";
+import { Text } from "@shared/ui/atoms";
 import ListItem, { Props as DefaultProps } from "@shared/ui/molecules/list-item";
 import ParkingCard, { Props as CardProps } from "@shared/ui/molecules/parking-card";
 import Ticket, { Props as TicketProps } from "@shared/ui/molecules/ticket";
 
-type ListGroupProps =
-  | { type: "card"; items: (CardProps & { id: string | number })[] }
-  | { type: "ticket"; items: (TicketProps & { id: string | number })[] }
-  | { type: "default"; items: (DefaultProps & { id: string | number })[] };
+type TitleType = "none" | "subtitle" | "title";
 
-export default function ListGroup({ type, items }: ListGroupProps) {
+interface BaseProps {
+  title?: string;
+  titleType?: TitleType;
+}
+
+type ListGroupProps =
+  | (BaseProps & { type: "card"; items: (CardProps & { id: string | number })[] })
+  | (BaseProps & { type: "ticket"; items: (TicketProps & { id: string | number })[] })
+  | (BaseProps & { type: "default"; items: (DefaultProps & { id: string | number })[] });
+
+export default function ListGroup({ type, items, title, titleType = "none" }: ListGroupProps) {
+  const renderTitle = {
+    subtitle: (
+      <Text className="text-neutral-850" typography="caption-sm">
+        {title}
+      </Text>
+    ),
+    title: (
+      <Text className="text-neutral-1000" typography="label-tight">
+        {title}
+      </Text>
+    ),
+  };
   return (
     <View className="flex-col gap-2">
+      {title && titleType !== "none" && renderTitle[titleType]}
       {items.map(({ id, ...props }) => {
         switch (type) {
           case "card":
