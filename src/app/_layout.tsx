b5ac2178bae0f397f5f491d00d79bdf87283652c
JGW-Korea <jgw6372@gmail.com>
2025-09-26 10:49:54 +0900

    remove: useMockServiceWorker 파일 삭제 및 _layout import 코드 삭제 (#134)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,9 @@
 import { Stack } from "expo-router";
-import { useMockServiceWorker } from "@global/hooks/useMockServiceWorker";
 import TanStackProvider from "@global/providers/TanStackProvider";
 import "@global/styles/global.css";
 import "@global/styles/nativewind-interop";
 
 export default function RootLayout() {
-  useMockServiceWorker(); // MSW 활성화 커스텀 훅 실행
-
   return (
     <TanStackProvider>
       <Stack screenOptions={{ headerShown: false }}>
