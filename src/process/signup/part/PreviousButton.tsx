0b5ebab7097af80e005b8e4863c01fe128b0e780
JGW-Korea <jgw6372@gmail.com>
2025-09-26 17:15:47 +0900

    style: Organism / Form(signup) PreviousButton 기본 palette 스타일 정의 (#124)

--- src/process/signup/part/PreviousButton.tsx
+++ src/process/signup/part/PreviousButton.tsx
@@ -1,5 +1,18 @@
 import { Button } from "@shared/ui/atoms";
 
+// 회원가입 이전 스텝 이동 버튼 컴포넌트
 export default function PreviousButton({ onPress }: { onPress: () => void }) {
-  return <Button variant="label" label="뒤로가기" onPress={onPress} />;
+  return (
+    <Button
+      variant="label"
+      label="뒤로가기"
+      onPress={onPress}
+      palette={{
+        bgColor: "bg-coolgray-100",
+        bgPressedColor: "bg-coolgray-100",
+        textColor: "text-neutral-900",
+        textPressedColor: "text-neutral-700",
+      }}
+    />
+  );
 }
