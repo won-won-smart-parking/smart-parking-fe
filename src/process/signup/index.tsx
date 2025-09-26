1a3a8d62033067755526965b4ab0f556ab9781ea
JGW-Korea <jgw6372@gmail.com>
2025-09-26 17:11:05 +0900

    feat: Organism / Form(signup) 회원가입 Process 버튼 + 스타일 구성 (#124)

--- src/process/signup/index.tsx
+++ src/process/signup/index.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { View } from "react-native";
 import SignUpAccount from "@features/auth/SignUpForm/SignUpAccount";
 import SignUpComplete from "@features/auth/SignUpForm/SignUpComplete";
 import SignUpVerification from "@features/auth/SignUpForm/SignUpVerification";
+import ContinueButton from "./part/ContinueButton";
+import PreviousButton from "./part/PreviousButton";
+import SubmitButton from "./part/SubmitButton";
 
 export type SignUpFormValues = {
   account: {
@@ -18,7 +21,9 @@ export type SignUpFormValues = {
 
 export default function SignUpForm() {
   const [step, setStep] = useState(1);
+
   const method = useForm<SignUpFormValues>({
+    mode: "onBlur",
     defaultValues: {
       account: {
         name: "",
@@ -31,17 +36,22 @@ export default function SignUpForm() {
     },
   });
 
-  useEffect(() => {
-    if (step === 0) setStep(1);
-  }, []);
+  const handleNext = () => setStep(step + 1); // 예시
+  const handlePrev = () => setStep(step - 1); // 예시
 
   return (
     <FormProvider {...method}>
-      <View>
-        <View>
+      <View className="flex-1 justify-between">
+        <View className="gap-6">
           {step === 1 && <SignUpAccount />}
           {step === 2 && <SignUpVerification />}
           {step === 3 && <SignUpComplete />}
+        </View>
+
+        {/* 각 스텝에 해당하는 버튼구성 레이아웃 */}
+        <View className="gap-3">
+          {step < 3 ? <ContinueButton onPress={handleNext} /> : <SubmitButton />}
+          {step > 1 && <PreviousButton onPress={handlePrev} />}
         </View>
       </View>
     </FormProvider>
