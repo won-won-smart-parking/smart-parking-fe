eca1e633de8b497fcf8f93c8d70e037123b74bda
JGW-Korea <jgw6372@gmail.com>
2025-09-26 15:16:47 +0900

    fix: Organism / Form(signup) 누락된 로직 수정 (#124)

--- src/process/signup/index.tsx
+++ src/process/signup/index.tsx
@@ -1,16 +1,49 @@
+import { useEffect, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+import { View } from "react-native";
 import SignUpAccount from "@features/auth/SignUpForm/SignUpAccount";
 import SignUpComplete from "@features/auth/SignUpForm/SignUpComplete";
 import SignUpVerification from "@features/auth/SignUpForm/SignUpVerification";
 
+export type SignUpFormValues = {
+  account: {
+    name: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
+    agreeLocation: string;
+    agreePushNotice: string;
+  };
+};
+
 export default function SignUpForm() {
-  const method = useForm();
+  const [step, setStep] = useState(1);
+  const method = useForm<SignUpFormValues>({
+    defaultValues: {
+      account: {
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+        agreeLocation: "",
+        agreePushNotice: "",
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (step === 0) setStep(1);
+  }, []);
 
   return (
     <FormProvider {...method}>
-      <SignUpAccount />
-      <SignUpVerification />
-      <SignUpComplete />
+      <View>
+        <View>
+          {step === 1 && <SignUpAccount />}
+          {step === 2 && <SignUpVerification />}
+          {step === 3 && <SignUpComplete />}
+        </View>
+      </View>
     </FormProvider>
   );
 }
