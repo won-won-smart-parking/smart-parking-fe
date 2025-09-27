ea083841e471a823ada5137877a3779de68750e7
JGW-Korea <jgw6372@gmail.com>
2025-09-27 20:45:10 +0900

    fix: 회원가입 단계 진행 관리 커스텀 훅 타입 에러 수정 (#124)

--- src/process/signup/useFormStep.ts
+++ src/process/signup/useFormStep.ts
@@ -1,17 +1,18 @@
 import { useState } from "react";
+import { Step } from "./index.type";
 
 // 회원가입 단계 진행을 관리하는 커스텀 훅
 export default function useFormStep() {
-  const [step, setStep] = useState(1); // 현재 단계 상태
+  const [step, setStep] = useState<Step>(1); // 현재 단계 상태
 
   // 다음 단계로 이동
   const handleNextStep = () => {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((step + 1) as Step);
   };
 
   // 이전 단계로 이동
   const handlePrevStep = () => {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((step - 1) as Step);
   };
 
   return { step, handleNextStep, handlePrevStep };
