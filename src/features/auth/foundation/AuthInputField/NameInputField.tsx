d7e58d49373efc8f532e62ab0e288f392389e8bb
JGW-Korea <jgw6372@gmail.com>
2025-10-02 22:22:37 +0900

    refactor: NameInputField TName 타입 매개변수 값 수정 (#124)

--- src/features/auth/foundation/AuthInputField/NameInputField.tsx
+++ src/features/auth/foundation/AuthInputField/NameInputField.tsx
@@ -1,12 +1,12 @@
 import { SignUpFormValues } from "@/process/signup";
 import FormInputController, { Props as FormInputControllerProps } from "../FormInputController";
 
-type Props = Omit<FormInputControllerProps<SignUpFormValues, "account.name">, "inputField" | "rules">;
+type Props = Omit<FormInputControllerProps<SignUpFormValues, "validation.name">, "inputField" | "rules">;
 
 // 이름 입력 필드 및 유효성 검사 규칙 관리 컴포넌트
 export default function NameInputField(props: Props) {
   return (
-    <FormInputController<SignUpFormValues, "account.name">
+    <FormInputController<SignUpFormValues, "validation.name">
       {...props}
       rules={{ required: { value: true, message: "필수 입력 항목입니다." } }}
       inputField={{
