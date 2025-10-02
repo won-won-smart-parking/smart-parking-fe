0c7808e736aaa2fd21531c9e5b2cfb3782631c10
JGW-Korea <jgw6372@gmail.com>
2025-10-02 22:13:23 +0900

    feat: Organism / Form(signup) 계속하기 버튼 press 이벤트 핸들러 로직 작성 (#124)

---
+++ src/process/signup/useFormValidity.ts
@@ -0,0 +1,28 @@
+import { SignUpFormValues } from ".";
+import { FieldErrors, useFormContext } from "react-hook-form";
+import { Step } from "./index.type";
+
+function isStepValid<K extends keyof SignUpFormValues>(values: SignUpFormValues[K], errors: FieldErrors<SignUpFormValues>, key: K) {
+  if (key === "account" && "agreePushNotice" in values) {
+    delete values["agreePushNotice"];
+  }
+
+  return Object.values(values ?? {}).every(Boolean) && !!!errors[key];
+}
+
+// 회원가입 각 단계 유효성 검증 성공 여부 처리 사용자 정의 훅
+// - 3단계는 정보 확인 단계이기 때문에 Submit 제출 제외한 검증 처리 없음
+export function useStepValidity(step: Omit<Step, 3>) {
+  const {
+    watch,
+    formState: { errors },
+  } = useFormContext<SignUpFormValues>();
+
+  // ( 이름, 이메일, 비밀번호, 약관 동의 ) 회원가입 단계 유효성 검사
+  if (step === 1) {
+    return isStepValid(watch("account"), errors, "account");
+  }
+
+  // (전화번호, 생년월일) 개인 정보 단계 유효성 검사
+  return isStepValid(watch("validation"), errors, "validation");
+}
