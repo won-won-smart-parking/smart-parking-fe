97b757db5ad69f5212962d186d3d02d555811ab2
JGW-Korea <jgw6372@gmail.com>
2025-10-02 22:14:15 +0900

    feat: Organism / Form(signup) 계속하기 버튼 컴포넌트 스타일 + 이벤트 핸들러 등록 (#124)

--- src/process/signup/part/ContinueButton.tsx
+++ src/process/signup/part/ContinueButton.tsx
@@ -1,5 +1,39 @@
 import { Button } from "@shared/ui/atoms";
+import { Step } from "../index.type";
+import { useStepValidity } from "../useFormValidity";
 
-export default function ContinueButton({ onPress }: { onPress: () => void }) {
-  return <Button variant="label" label="계속하기" onPress={onPress} />;
+interface Props {
+  step: Step;
+  onPress: () => void;
+}
+
+// 계속하기 버튼 로직 단계 (각 단계 유효성 검사 통과 시 단계 증가)
+// - Step 01. RHF.defaultValues.account 검사 수행
+// - Step 02. RHF.defaultValues.verification 검사 수행
+export default function ContinueButton({ step, onPress }: Props) {
+  const isValid = useStepValidity(step);
+
+  return (
+    <Button
+      variant="label"
+      label="계속하기"
+      onPress={onPress}
+      palette={
+        isValid
+          ? {
+              bgColor: "bg-blue-300",
+              bgPressedColor: "bg-blue-400",
+              textColor: "text-neutral-100",
+              textPressedColor: "text-neutral-300",
+            }
+          : {
+              bgColor: "bg-neutral-500",
+              bgPressedColor: "bg-neutral-600",
+              textColor: "text-neutral-870",
+              textPressedColor: "text-neutral-800",
+            }
+      }
+      disabled={!isValid}
+    />
+  );
 }
