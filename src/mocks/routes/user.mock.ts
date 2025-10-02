524d57f4311be366bf50dc93b759d19f125a1530
JGW-Korea <jgw6372@gmail.com>
2025-10-02 22:08:49 +0900

    comment: User Mock API 주석 내용 추가 (#124)

--- src/mocks/routes/user.mock.ts
+++ src/mocks/routes/user.mock.ts
@@ -1,9 +1,13 @@
 import { Response, Server } from "miragejs";
 
+/**
+ * User Mock API
+ * 로그인, 로그아웃, 회원가입 등의 API를 흉내내는 Mock Object를 구성
+ */
 export function userMockRoutes(server: Server) {
   // Email 중복 검사 API
   server.get("/api/auth/emails", (schema, request) => {
-    const { email } = request.queryParams as { email?: string };
+    const { email } = request.queryParams as { email: string };
 
     // Email 중복 검사
     if (schema.db.users.findBy({ email })) {
