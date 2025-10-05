dc3aaff0f5d88e956214b49a990751e3b5f22faa
JGW-Korea <jgw6372@gmail.com>
2025-10-05 20:52:24 +0900

    remove: MirageJS 테스트 연동 movie mock object 제거 및 server.ts 코드 타입 수정 (#124)

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,6 +1,5 @@
 import { createServer, Model } from "miragejs";
 import userData from "./data/user.json";
-import { movieMockRoutes } from "./routes/movie.mock";
 import { userMockRoutes } from "./routes/user.mock";
 
 // MirageJS Mock Server 구동 유틸 함수
@@ -11,13 +10,12 @@ export default function enableMockServer() {
       user: Model,
     },
     seeds(server) {
-      userData.forEach((data) => server.create("user", data));
+      userData.forEach((data) => server.create("user", data as Record<string, unknown>));
     },
     routes() {
       this.urlPrefix = process.env.EXPO_PUBLIC_API_URL as string; // url 접두사 지정
 
       // API 문서를 통해 구성한 가짜 Mock API 호출
-      movieMockRoutes(this);
       userMockRoutes(this);
     },
   });
