d1c364de7498aaa721a8670a60dc0a6eda8064e8
JGW-Korea <jgw6372@gmail.com>
2025-10-05 21:08:51 +0900

    style: 회원가입 제출 버튼 스타일 수정 (#124)

--- src/process/signup/part/SubmitButton.tsx
+++ src/process/signup/part/SubmitButton.tsx
@@ -1,8 +1,21 @@
 import { Button } from "@shared/ui/atoms";
 import useFormSubmit from "../useFormSubmit";
 
+// 회원가입 제출(Submit) 버튼
 export default function SubmitButton() {
   const { handleSubmit } = useFormSubmit();
 
-  return <Button variant="label" label="로그인 화면으로 이동" onPress={handleSubmit} />;
+  return (
+    <Button
+      variant="label"
+      label="로그인 화면으로 이동"
+      onPress={handleSubmit}
+      palette={{
+        bgColor: "bg-blue-300",
+        bgPressedColor: "bg-blue-400",
+        textColor: "text-neutral-100",
+        textPressedColor: "text-neutral-300",
+      }}
+    />
+  );
 }
