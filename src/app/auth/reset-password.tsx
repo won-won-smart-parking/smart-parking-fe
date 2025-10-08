90e1f5e5044284dc7ce0d30463778fcce3de0077
JGW-Korea <jgw6372@gmail.com>
2025-10-08 23:23:46 +0900

    test: 회원가입, 로그인, 비밀번호 찾기 라우트 구성한 Form 연동 확인 테스트 (#124)

--- src/app/auth/reset-password.tsx
+++ src/app/auth/reset-password.tsx
@@ -1,11 +1,13 @@
 import { View } from "react-native";
+import ResetPassword from "@features/auth/form/ResetPassword";
 import { Text } from "@shared/ui/atoms";
 
 // smartparking://profile/edit
 export default function ResetPasswordScreen() {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1">
       <Text typography="display-default">비밀번호 수정 페이지</Text>
+      <ResetPassword />
     </View>
   );
 }
