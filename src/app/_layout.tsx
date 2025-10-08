c638beb385de83afc23b88bc8811c693765623ef
JGW-Korea <jgw6372@gmail.com>
2025-10-08 15:19:34 +0900

    refactor: 메인 레이아웃 사용자 정보 초기화 사이드 이펙트 로직 추가 (#124)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,18 @@
 import Drawer from "expo-router/drawer";
+import { useEffect } from "react";
+import { useUserStore } from "@entities/user/user.store";
 import TanStackProvider from "@global/providers/TanStackProvider";
 import "@global/styles/global.css";
 import "@global/styles/nativewind-interop";
 import SideMenu from "@widgets/home/side-menu";
 
 export default function RootLayout() {
+  // 사용자 정보 초기화 사이드 이펙트 수행
+  const userLoginInit = useUserStore((state) => state.init);
+  useEffect(() => {
+    userLoginInit();
+  }, [userLoginInit]);
+
   return (
     <TanStackProvider>
       <Drawer
