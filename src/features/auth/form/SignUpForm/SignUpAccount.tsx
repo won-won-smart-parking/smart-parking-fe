ae7e1db0d2a44a5d7fb85cd07aa85dcc6305c0a0
JGW-Korea <jgw6372@gmail.com>
2025-10-08 23:23:02 +0900

    update: Organism / Form(signup) 수정된 Input Field 속성 구조로 변경 (#124)

--- src/features/auth/form/SignUpForm/SignUpAccount.tsx
+++ src/features/auth/form/SignUpForm/SignUpAccount.tsx
@@ -5,7 +5,7 @@ import { EmailInputField, LocationAgreement, PasswordConfirmInputField, Password
 
 // 이메일, 비밀번호, 비밀번호 확인 입력 필드를 구성하는 레이아웃
 export default function SignUpAccount() {
-  const { control, resetField } = useFormContext<SignUpFormValues>(); // 상위 RHF 제공자(Provider) 구독
+  const { control, resetField, watch } = useFormContext<SignUpFormValues>(); // 상위 RHF 제공자(Provider) 구독
 
   // 각 스텝 별 회원가입 레이아웃 구조는 모두 동일하기 때문에 상위 컴포넌트에서 레이아웃으로 관리를 하고,
   // 각 스텝 별 레이아웃 구조에서는 JSX 규칙에 따라 하나의 부모가 있어야 하는 조건에 따라 React.Fragment로 관리한다.
@@ -14,7 +14,12 @@ export default function SignUpAccount() {
       <View className="gap-4">
         <EmailInputField control={{ name: "account.email", control }} resetField={resetField} duplicateValid />
         <PasswordInputField control={{ name: "account.password", control }} resetField={resetField} />
-        <PasswordConfirmInputField control={{ name: "account.passwordConfirm", control }} resetField={resetField} />
+        <PasswordConfirmInputField
+          control={{ name: "account.passwordConfirm", control }}
+          resetField={resetField}
+          watch={watch}
+          confirmTarget="account.password"
+        />
       </View>
 
       <View className="gap-5">
