408e058e8c0b1252a1a1f51c266998c7aa421b82
JGW-Korea <jgw6372@gmail.com>
2025-10-08 16:44:26 +0900

    remove: 기존 Organism / Signin 관련 로직 FormInputField 파일 제거 (#124)

--- src/features/auth/foundation/FormInputField.tsx
+++
@@ -1,5 +0,0 @@
-import InputField, { Props as InputFieldProps } from "@shared/ui/molecules/input-field";
-
-export default function FormInputField({ title, input, message, button }: InputFieldProps) {
-  return <InputField title={title} input={input} message={message} button={button} />;
-}
