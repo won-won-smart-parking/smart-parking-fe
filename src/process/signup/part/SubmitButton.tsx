bb065fb5ea6dda7dab65ae36b677afcc71e98fb1
JGW-Korea <jgw6372@gmail.com>
2025-10-08 15:37:40 +0900

    refactor: 회원가입 기반 제출 버튼 -> 로그인 + 회원가입 범용적 제출 버튼으로 재구성 (#124)

--- src/process/signup/part/SubmitButton.tsx
+++ src/process/signup/part/SubmitButton.tsx
@@ -1,21 +1,25 @@
 import { Button } from "@shared/ui/atoms";
-import useFormSubmit from "../useFormSubmit";
 
-// 회원가입 제출(Submit) 버튼
-export default function SubmitButton() {
-  const { handleSubmit } = useFormSubmit();
+interface Props {
+  label: string;
+  onPress: () => void;
+}
 
+// 로그인 + 회원가입 폼 제출(Submit) 버튼
+export default function SubmitButton({ label, onPress }: Props) {
   return (
     <Button
       variant="label"
-      label="로그인 화면으로 이동"
-      onPress={handleSubmit}
+      label={label}
+      onPress={onPress}
       palette={{
         bgColor: "bg-blue-300",
         bgPressedColor: "bg-blue-400",
         textColor: "text-neutral-100",
         textPressedColor: "text-neutral-300",
       }}
+      fullWidth
+      disablePressedEffect
     />
   );
 }
