cf0d13a64bd7ca8c2bdf96e5f57b6a00a57f0373
JGW-Korea <jgw6372@gmail.com>
2025-10-16 18:53:17 +0900

    test: Organism / Map Overlay web-view 연동 테스트 + 바텀 시트 요소 쌓임 순서 확인 (#128)

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,85 +1,47 @@
-// import axios from "axios";
-// import { useNavigation } from "expo-router";
-// import { useEffect } from "react";
-// import { Button, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-// import { create } from "zustand";
-import SignUpForm from "@/process/signup";
+import { useMemo, useRef, useState } from "react";
+import { Dimensions, View } from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import { WebView } from "react-native-webview";
+import ParkingListSheet from "@features/pass/bottom-sheet/ParkingListSheet";
+import BottomSheet from "@gorhom/bottom-sheet";
 
-// import { Text } from "@shared/ui/atoms";
-
-// import { useQuery } from "@tanstack/react-query";
-
-// const fetchUser = async () => {
-//   const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-//   return response;
-// };
-
-// interface ZustandStateProps {
-//   count: number;
-//   increase: () => void;
-//   decrease: () => void;
-// }
-
-// const useZustandStore = create<ZustandStateProps>((set, get) => {
-//   return {
-//     count: 1,
-//     increase() {
-//       const { count } = get();
-//       set({ count: count + 1 });
-//     },
-//     decrease() {
-//       const { count } = get();
-//       set({ count: count - 1 });
-//     },
-//   };
-// });
-
-// const instance = axios.create({
-//   adapter: "fetch",
-//   baseURL: "https://api.example.com",
-// });
-
-// smartparking://
 export default function MainScreen() {
-  // const count = useZustandStore((state) => state.count);
+  const sheetRef = useRef<BottomSheet>(null);
 
-  // useEffect(() => {
-  //   async function fetchAPI() {
-  //     try {
-  //       const response = await instance.get("/api/movies");
+  // 홈 라우트 높이 계산 로직
+  const { height } = Dimensions.get("screen");
+  const { top, bottom } = useSafeAreaInsets();
+  const [headerHeight, setHeaderHeight] = useState<number>(0);
 
-  //       console.log(response);
-  //     } catch (error) {
-  //       if (error instanceof Error) {
-  //         console.log(error.name);
-  //       }
-  //     }
-  //   }
+  const snapPoints = useMemo(() => {
+    if (!headerHeight) {
+      return ["25%"];
+    }
 
-  //   fetchAPI();
-  // }, []);
-
-  // const { isLoading, isError } = useQuery({
-  //   queryKey: ["users"],
-  //   queryFn: fetchUser,
-  // });
-
-  // if (isLoading) return <Text>Lodaing...</Text>;
-  // if (isError) return <Text>Error...</Text>;
-
-  // const navigation = useNavigation();
+    // 1. 모바일 기기에 따른 height(뷰포트 높이)를 구한다.
+    // 2. SafeArea 영역의 노치 영역을 구한다.
+    // 3. 검색 컨테이너의 Y + 레이아웃 높이를 구하여 레이아웃의 끝단을 위치를 구한다.
+    // 4. 1, 2, 3번을 통해 구한 값들과 12(검색 컨테이너와 바텀 시트 간의 간격)을 빼서 각 모바일 기기에 대응할 수 있는 정확한 바텀 시트의 최대 지점 높이를 구한다.
+    const maxSnap = height - top - bottom - headerHeight - 12;
+    return [height * 0.25, maxSnap];
+  }, [headerHeight, top, bottom, height]);
 
   return (
-    <SafeAreaView className="flex-1">
-      {/* <Text typography="display-default">메인 페이지{count}</Text> */}
+    <SafeAreaView className="flex-1 items-center justify-center">
+      {/* 임시 검색 컨테이너 (주차장 리스트 바텀 시트 최대 높이 지점을 구하기 위함) */}
+      <View
+        className="absolute top-24 h-14 w-full bg-blue-300"
+        onLayout={(e) => {
+          const { y, height: layoutHeight } = e.nativeEvent.layout;
+          setHeaderHeight(y + layoutHeight);
+        }}
+      />
 
-      {/* 
-        navigation.openDrawer를 통해 사이드메뉴를 열 수 있다.
-        [공식 문서 참고](https://reactnavigation.org/docs/drawer-based-navigation/)
-      */}
-      {/* <Button title="Open drawer menu!!" onPress={() => navigation.openDrawer()} /> */}
-      <SignUpForm />
+      <View className="absolute bottom-0 top-0 w-full bg-neutral-900">
+        <WebView source={{ uri: process.env.EXPO_PUBLIC_KAKAO_MAP_WEB_VIEW_URL as string }} />
+      </View>
+
+      <ParkingListSheet ref={sheetRef} snapPoints={snapPoints} />
     </SafeAreaView>
   );
 }
